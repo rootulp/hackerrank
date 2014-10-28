e1f50800e5a8b69436693ca170b1b8edbe4e4322
Rootul <rootulp@gmail.com>
2014-10-28 17:04:17 -0400

    complete manasa and stones

---
+++ manasa_and_stones.rb
@@ -0,0 +1,16 @@
+def last_stone(stones, a, b)
+    results = []
+    for i in 0..stones do 
+        results << (i*a) + ((stones-i)*b)
+        results << (i*b) + ((stones-i)*a)
+    end
+    puts results.uniq.sort.join(" ")
+end
+
+t = gets.to_i
+t.times{
+    stones = gets.to_i
+    a = gets.to_i
+    b = gets.to_i
+    last_stone(stones-1, a, b)
+}
