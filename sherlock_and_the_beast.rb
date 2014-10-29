785519c6dd5ea9c124aa923eda7618ecd6bcb6cb
Rootul <rootulp@gmail.com>
2014-10-29 15:41:43 -0400

    creates possible solutions now creates largest possible valid nums

--- sherlock_and_the_beast.rb
+++ sherlock_and_the_beast.rb
@@ -1,10 +1,9 @@
-def create_possible_solutions(curr_num, solution_length, possible_solutions)
-    if curr_num.length == solution_length
-        possible_solutions << curr_num
-    else
-        create_possible_solutions(curr_num + "3", solution_length, possible_solutions)
-        create_possible_solutions(curr_num + "5", solution_length, possible_solutions)
-    end
+def create_possible_solutions(solution_length)
+  possible_solutions = []
+  for i in 0..solution_length do 
+    possible_solutions << "5" * i + "3" * (solution_length - i)
+  end
+  return possible_solutions
 end
 
 def test_possible_solutions(possible_solutions)
@@ -38,8 +37,7 @@ end
 t = gets.to_i
 t.times{
     solution_length = gets.to_i
-    possible_solutions = []
-    create_possible_solutions("", solution_length, possible_solutions)
+    possible_solutions = create_possible_solutions(solution_length)
     solutions = test_possible_solutions(possible_solutions)
     print_solution(solutions)
 }
