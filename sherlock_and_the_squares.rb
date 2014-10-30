01124435c8e26618be1860e0926f9a18fc90d3c8
Rootul <rootulp@gmail.com>
2014-10-30 17:12:32 -0400

    initial solution

--- sherlock_and_the_squares.rb
+++ sherlock_and_the_squares.rb
@@ -1,18 +1,7 @@
-def squares_between(a, b)
-  perfect_squares = []
-  for i in a..b
-    perfect_squares << i if is_square?(i)
-  end
-  return perfect_squares.size
-end
-
-def is_square?(x)
-  return true if x**0.5 % 1 == 0
-  false
-end
-
 t = gets.to_i
 t.times{
   (a, b) = gets.split.map{|i| i.to_i}
-  puts squares_between(a,b)
+  small_a = ((a-1)**0.5).floor
+  small_b = (b**0.5).floor
+  puts small_b - small_a
 }
