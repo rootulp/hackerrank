92b1bfdc8b183a30af29bd3e460a3e1d78b69943
Rootul <rootulp@gmail.com>
2014-10-30 14:38:02 -0400

    complete service lane

---
+++ service_lane.rb
@@ -0,0 +1,11 @@
+def max_width(entry, exit, highway)
+  subarray = highway[entry..exit]
+  puts subarray.min
+end
+
+(n, t) = gets.split.map{|i| i.to_i}
+highway = gets.split.map{|i| i.to_i}
+t.times {
+  (entry, exit) = gets.split.map{|i| i.to_i}
+  max_width(entry, exit, highway)
+}
