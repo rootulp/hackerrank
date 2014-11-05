1f5af176dac6af319569c6508f87cb6062855e9c
Rootul <rootulp@gmail.com>
2014-11-05 16:10:42 -0500

    better implementation

--- filling-jars.rb
+++ filling-jars.rb
@@ -1,11 +1,8 @@
 (size, operations) = gets.split.map{|i| i.to_i}
-jars = Array.new(size, 0)
+total = 0
 operations.times do
     (start, stop, value) = gets.split.map{|i| i.to_i}
-    jars.each_index do |i|
-        if i >= start-1 and i <= stop-1
-            jars[i] = jars[i] + value
-        end
-    end
+    num = stop - start + 1
+    total += num * value
 end
-puts jars.inject{ |sum, element| sum + element }.to_i / jars.size
+puts total / size
