58ab92dd5897d1f46ad52925c6de403d980b5ebc
Rootul <rootulp@gmail.com>
2014-11-05 15:46:04 -0500

    refactor find digits

--- find-digits.rb
+++ find-digits.rb
@@ -2,11 +2,9 @@ t = gets.chomp.to_i
 t.times do
     total = 0
     n = gets.chomp.to_i
-    n_arr = n.to_s.split(//).map{|i| i.to_i}
-    n_arr.each do |digit|
-        if n % digit == 0
-            total += 1
-        end
+    n.to_s.split(//).map{|i| i.to_i}.each do |digit|
+        next if digit == 0
+        total += 1 if n % digit == 0
     end
     puts total
 end
