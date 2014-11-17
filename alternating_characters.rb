d2ac6bd3324642b44398e013a961a8c30471a708
Rootul Patel <rootulp@gmail.com>
2014-11-17 15:17:19 -0500

    alternating characters solution

---
+++ alternating_characters.rb
@@ -0,0 +1,16 @@
+def solve(characters)
+  count = 0
+  characters.each_with_index do |character, index|
+    if index != characters.length
+      count += 1 if character == characters[index+1]
+    end
+  end
+  
+  return count
+end
+
+t = gets.to_i
+t.times {
+  characters = gets.chomp.split(//)
+  puts solve(characters)
+}
