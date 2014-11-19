c65e81cbacd9d0aaa85d568218598fadef3366ec
Rootul Patel <rootulp@gmail.com>
2014-11-19 15:19:38 -0500

    maximizing xor solution

---
+++ maximizing_xor.rb
@@ -0,0 +1,16 @@
+def maxXor(l, r)
+    max = 0
+    lr = (l..r).to_a
+    combinations = lr.combination(2).to_a
+
+    combinations.each do |x|
+        curr_xor = x[0] ^ x[1]
+        max = curr_xor if curr_xor > max
+    end
+    
+    return max
+end
+
+l = gets.to_i
+r = gets.to_i
+print maxXor(l, r)
