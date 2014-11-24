5e11cae66681e00500ae0eb42470ba6c95a5e488
Rootul Patel <rootulp@gmail.com>
2014-11-24 16:50:54 -0500

    complete quicksort1 partition

---
+++ quicksort1_partition.rb
@@ -0,0 +1,15 @@
+def  partition(ar) 
+    p = ar.shift
+    a = []
+    b = []
+    ar.each do |val|
+        a << val if val < p
+        b << val if val >= p
+    end
+    result = a << p << b
+    return result.join(" ")
+end
+
+cnt = gets.to_i;
+ar = gets.chomp.split.map{|x| x.to_i};
+puts partition(ar);
