815d6bbc6d73e0d3d564bde6d8ca26f06fbbe6c1
Rootul Patel <rootulp@gmail.com>
2014-11-28 02:29:35 -0500

    quick and dirty solution closest numbers

---
+++ closest_numbers.rb
@@ -0,0 +1,18 @@
+n = gets.chomp
+nums = gets.split().map {|x| x.to_i}.sort!
+
+min_dif = nums.max
+closest = []
+nums.each_with_index do |num, index|
+    if index != 0
+        if num - nums[index-1] == min_dif
+            closest << nums[index-1] 
+            closest << num
+        elsif num - nums[index-1] < min_dif
+            min_dif = num - nums[index-1]
+            closest = [num, nums[index-1]]
+        end
+    end
+end
+
+puts closest.sort!.join(" ")
