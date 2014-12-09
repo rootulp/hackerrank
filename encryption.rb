4f01597f67c079012c3ce4ca146a2064afc9c874
Rootul Patel <rootulp@gmail.com>
2014-12-09 17:15:18 -0500

    short and sweet solution

---
+++ encryption.rb
@@ -0,0 +1,10 @@
+rectangle = []
+result = ""
+
+phrase = gets.gsub(/\s+/, "").split(//)
+cols = (phrase.length**0.5).ceil
+phrase.each_slice(cols) {|row| rectangle << row}
+encoded = rectangle.first.zip(*rectangle.drop(1))
+encoded.each {|col| result << col.join() + " "} 
+
+puts result
