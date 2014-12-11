3620c9815d5c5069b142c308a689523f1aad97c4
Rootul Patel <rootulp@gmail.com>
2014-12-11 03:56:01 -0500

    short lonely integer python implementation

---
+++ Python/lonely_integer.py
@@ -0,0 +1,9 @@
+def lonelyinteger(nums):
+    for i in range(len(nums)):
+        if nums.count(nums[i]) == 1:
+            return nums[i]
+
+if __name__ == '__main__':
+    a = input()
+    nums = map(int, raw_input().strip().split(" "))
+    print lonelyinteger(nums)
