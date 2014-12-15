413e9a9e5dfd7a6fcc4adb5e718a10fbdb271788
Rootul Patel <rootulp@gmail.com>
2014-12-14 20:21:37 -0500

    anagram solution isnt efficent enough

---
+++ Python/anagram.py
@@ -0,0 +1,23 @@
+def anagram(str):
+    if len(str) % 2 == 1:
+        return "-1"
+    else:
+        str_arr = []
+        str_arr.extend(str)
+        a = str_arr[:len(str_arr)/2]
+        b = str_arr[(len(str_arr)/2):]
+        
+        count = 0
+        for index in range(len(a)):
+            if b.count(a[index]) != 0:
+                b.remove(a[index])
+            else:
+                count += 1
+
+        return count
+
+test_cases = int(raw_input())
+
+for x in range(test_cases):
+    str = raw_input()
+    print anagram(str)
