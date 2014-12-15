6336b7dd3a432d7c126a945a02f11e1e6512a49d
Rootul Patel <rootulp@gmail.com>
2014-12-14 20:48:27 -0500

    more efficent solution

--- Ruby/anagram.rb
+++ Ruby/anagram.rb
@@ -1,14 +1,26 @@
 def anagram_solver(word)
+
     return "-1" if word.length % 2 == 1
+
     middle = word.length / 2
-    a = word[0..(middle - 1)].split(//)
-    b = word[middle..-1].split(//)
-    a.each do |char|
-        b.delete_at(b.index(char)) if b.include?(char)
+    left = word[0..(middle - 1)].split(//).sort
+    right = word[middle..-1].split(//).sort
+
+    if left == right
+      return "0"
+    else 
+      left.each do |char|
+        index = right.index(char)
+        right.delete_at(index) unless index.nil?
+      end
+
+      return right.length
     end
-    return b.length
+    
 end
+
 t = gets.to_i
+
 t.times do 
     puts anagram_solver(gets.chomp)
 end
