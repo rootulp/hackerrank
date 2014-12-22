3e6be2a241b3524e1529262de98019aad61e1dfe
Rootul Patel <rootulp@gmail.com>
2014-12-22 00:21:39 -0500

    initial sollution

--- Ruby/insertion_sort_part1.rb
+++ Ruby/insertion_sort_part1.rb
@@ -1,5 +1,26 @@
 def  insertionSort(ar)
+    ar.reverse!
+    v = ar[0]
     
+    if v >= ar[1]
+        puts ar.reverse.join(" ")
+        return
+    end
+        
+    ar.each_with_index do |x ,index|
+        if index == ar.length-1
+            ar[index] = v
+            puts ar.reverse.join(" ")
+            return
+        elsif v > ar[index+1]
+            ar[index] = v
+            puts ar.reverse.join(" ")
+            return
+        else
+            ar[index] = ar[index+1]
+            puts ar.reverse.join(" ")
+        end
+    end     
 end
 
 count = gets.to_i
