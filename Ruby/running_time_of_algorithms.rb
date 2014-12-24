1db3046383a62bc100f33b37d88cd9f0355013d9
Rootul Patel <rootulp@gmail.com>
2014-12-24 14:30:47 -0500

    change insertion sort implementation to count number of shifts

---
+++ Ruby/running_time_of_algorithms.rb
@@ -0,0 +1,20 @@
+def  insertionSort(ar)
+  shifts = 0
+  (1..ar.length-1).each do |i|
+    
+    val = ar.delete_at(i)
+    index = i
+    
+    while index > 0 && val < ar[index -1]
+      index -= 1 
+      shifts += 1
+    end
+    
+    ar.insert(index, val)
+  end
+  puts shifts
+end
+
+count = gets.to_i
+ar = gets.strip.split.map {|i| i.to_i}
+insertionSort(ar)
