30398bea4f990411bccd1d110e6ce508b826ba62
Rootul Patel <rootulp@gmail.com>
2015-01-03 16:10:28 -0500

    refactor closest_number

--- Ruby/closest_number.rb
+++ Ruby/closest_number.rb
@@ -2,24 +2,16 @@ t = gets.to_i
 t.times do 
     (a,b,x) = gets.split(" ").map{|x| x.to_i}
     num_to_find = a**b
+    low = 0
     high = x
-    low = high
-        
-    while high < a**b
+    
+    while high < num_to_find
         low = high
         high += x
     end
     
     diff_low = (num_to_find - low).abs
     diff_high = (high - num_to_find).abs
-    diff_zero = (num_to_find).abs
-    
-    if diff_zero < diff_low && diff_zero < diff_high
-        puts "0"
-    elsif diff_low < diff_zero && diff_low < diff_high
-        puts low
-    else
-        puts high
-    end
-      
+
+    puts diff_low < diff_high ? low : high
 end
