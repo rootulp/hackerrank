3af503745a0bc4775718e79b0d5ac92d37e9b9db
Rootul Patel <rootulp@gmail.com>
2015-02-09 01:41:43 -0500

    solve sherlock and pairs using occurences hash

---
+++ Ruby/sherlock_and_pairs.rb
@@ -0,0 +1,29 @@
+class SherlockAndPairs
+  attr_reader :arr, :counts
+  def initialize(elements)
+    @arr = elements.split(" ").map{|x| x.to_i}
+    @counts = find_counts
+  end
+
+  def pairs
+    total = 0
+    counts.each do |key, value|
+      next if value == 1
+      total += value * (value - 1)
+    end
+    total
+  end
+
+  def find_counts
+    counts = Hash.new 0
+    arr.each {|x|counts[x] += 1}
+    counts
+  end
+end
+
+num_cases = gets.to_i
+num_cases.times do
+  n = gets.to_i
+  test_case = SherlockAndPairs.new(gets)
+  puts test_case.pairs
+end
