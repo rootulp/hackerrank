3ec6c0de79af6f72b362fb124c7a19b9e661be15
rootulp <rootulp@gmail.com>
2015-02-10 13:01:02 -0500

    refactor solution and add comments

--- Ruby/quicksort_in_place.rb
+++ Ruby/quicksort_in_place.rb
@@ -5,23 +5,27 @@ class QuickSort
   end
 
   def sort(first_i=0, last_i=arr.size-2)
+    # Stop if already sorted
     return if first_i > last_i
 
+    # Declare index var's for easier ref
     insert_i = first_i
     pivot_i = last_i + 1
     
+    # Partition
     (first_i..last_i).each do |i|
       if arr[i] < arr[pivot_i]
+        # Move smaller elements to left
         swap(i, insert_i)
         insert_i += 1
       end
     end
+    # Move pivot inbetween left(smaller elements) & right(larger elements)
     swap(pivot_i, insert_i)
     print_arr
 
-    #puts "#{first_i}, #{insert_i-2}"
+    # Recursively sort left and ride sides
     sort(first_i, insert_i-2)
-    #puts "#{insert_i+1}, #{pivot_i-1}"
     sort(insert_i+1, last_i)
   end
 
