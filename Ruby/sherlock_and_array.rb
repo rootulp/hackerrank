1fa3163895a5aa99c3995234567fc3a60bce54d7
rootulp <rootulp@gmail.com>
2015-02-10 16:55:25 -0500

    sherlock and array implementation
    does not pass test case 6

---
+++ Ruby/sherlock_and_array.rb
@@ -0,0 +1,31 @@
+class SherlockAndArray
+  attr_reader :arr
+  def initialize(elements)
+    @arr = elements.split(" ").map{|x| x.to_i}
+  end
+
+  def solve
+    valid_mid? ? "YES" : "NO"
+  end
+
+  def valid_mid?
+    left = 0
+    right = arr.reduce(:+)
+
+    arr.each_index do |i|
+      right -= arr[i]
+      next if i == 0
+      left += arr[i-1]
+      return true if left == right
+    end
+
+    false
+  end
+
+end
+
+num_cases = gets.to_i
+num_cases.times do
+  num_elements = gets.to_i
+  puts SherlockAndArray.new(gets).solve
+end
