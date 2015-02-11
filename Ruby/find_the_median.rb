0afd87fb72742b4b1f664b398c9be2b0fb7a0194
rootulp <rootulp@gmail.com>
2015-02-11 03:03:27 -0500

    solve find the median

---
+++ Ruby/find_the_median.rb
@@ -0,0 +1,45 @@
+class FindTheMedian
+  attr_accessor :arr
+  def initialize(elements)
+    @arr = elements.split(" ").map{|x| x.to_i};
+  end
+
+  def find_median(first_i=0, last_i=arr.size-2)
+    median_i = (arr.size-1) / 2
+
+    # Stop if already sorted
+    return arr[median_i] if first_i > last_i
+
+    # Declare index var's for easier ref
+    insert_i = first_i
+    pivot_i = last_i + 1
+    
+    # Partition
+    (first_i..last_i).each do |i|
+      if arr[i] < arr[pivot_i]
+        # Move smaller elements to left
+        swap(i, insert_i)
+        insert_i += 1
+      end
+    end
+    # Move pivot inbetween left(smaller elements) & right(larger elements)
+    swap(pivot_i, insert_i)
+    
+    if insert_i == median_i
+      return arr[insert_i]
+    elsif insert_i > median_i
+      find_median(first_i, insert_i-2)
+    elsif insert_i < median_i
+      find_median(insert_i+1, last_i)
+    end
+  end
+
+  private
+
+  def swap(a, b)
+    arr[a], arr[b] = arr[b], arr[a]
+  end
+end
+
+n = gets.to_i
+puts FindTheMedian.new(gets).find_median
