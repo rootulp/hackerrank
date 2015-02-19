07bd458c8e25f3e8f21f93005f9c3edf732b16b3
rootulp <rootulp@gmail.com>
2015-02-19 12:24:11 -0500

    refactor and reformat insertion sort into class

--- Ruby/insertion_sort_part2.rb
+++ Ruby/insertion_sort_part2.rb
@@ -1,15 +1,20 @@
-def  insertionSort(ar)
-    (1..ar.length-1).each do |i|
-        val = ar.delete_at(i)
+class InsertionSort
+  attr_reader :arr
+  def initialize(str)
+    @arr = str.strip.split.map {|x| x.to_i}
+  end
 
-        index = i
-        index -= 1 while index > 0 && val < ar[index -1]
-
-        ar.insert(index, val)
-        puts ar.join(" ")
+  def  sort
+    arr.each_index do |index|
+      next if index == 0
+      val = arr.delete_at(index)
+      index -= 1 while index > 0 && val < arr[index-1]
+      arr.insert(index, val)
+      puts arr.join(" ")
+    end
   end
 end
 
-count = gets.to_i
-ar = gets.strip.split.map {|i| i.to_i}
-insertionSort(ar)
+arr_size = gets.to_i
+test_case = InsertionSort.new(gets)
+test_case.sort
