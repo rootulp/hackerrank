51f2143e9852c19791b1334ddb77166fb5468fae
rootulp <rootulp@gmail.com>
2015-02-19 12:11:42 -0500

    solve counting sort 1

---
+++ Ruby/counting_sort1.rb
@@ -0,0 +1,23 @@
+class CountingSort
+  attr_reader :arr, :counts
+  def initialize(elements)
+    @arr = elements.split(" ").map{|x| x.to_i}
+    @counts = build_counts
+  end
+  
+  def build_counts
+    counts = Hash.new(0)
+    arr.each {|val| counts[val] += 1}
+    counts 
+  end
+  
+  def print_counts
+    (0..99).each do |val|
+        print "#{counts[val]} "
+    end
+  end
+end
+
+arr_size = gets.to_i
+test_case = CountingSort.new(gets.chomp)
+test_case.print_counts
