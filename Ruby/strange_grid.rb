06cfae6efaaa5971e4aaa95e3a92035a8e73899f
Rootul Patel <rootulp@gmail.com>
2015-04-30 17:31:06 -0400

    Solve strange_grid
    Solution is short and dirty but strange grid is just a math puzzle
    involving row / col indexes.

---
+++ Ruby/strange_grid.rb
@@ -0,0 +1,11 @@
+class StrangeGrid
+  def self.val_at(row, col)
+    suffix = col * 2 - 1
+    suffix -= 1 if row % 2 == 1
+    prefix = (row - 1) / 2
+    "#{prefix}#{suffix}".to_i
+  end
+end
+
+row, col = gets.split(' ').map(&:to_i)
+puts StrangeGrid.val_at(row, col)
