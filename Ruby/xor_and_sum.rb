95dcf44c05e3b673bf361bc08d17cf2f27f6dc53
Rootul Patel <rootulp@gmail.com>
2015-05-07 00:28:33 -0400

    initial solution for xor_and_sum

---
+++ Ruby/xor_and_sum.rb
@@ -0,0 +1,15 @@
+class XorAndSum
+  UPPER_BOUND = 314159
+  MODULO_VAL = (10**9) + 7
+
+  def self.calculate(a, b)
+    sum = 0
+    (0..UPPER_BOUND).each { |i| sum += (a ^ (b << i)) }
+    sum % MODULO_VAL
+  end
+
+end
+
+a = gets.to_i(2)
+b = gets.to_i(2)
+puts XorAndSum.calculate(a, b)
