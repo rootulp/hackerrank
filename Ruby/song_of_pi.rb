9e67748432d23724f61ca2b6873206a3b6fb85cc
Rootul Patel <rootulp@gmail.com>
2015-05-13 19:02:20 -0400

    quick solution for song_of_pi

---
+++ Ruby/song_of_pi.rb
@@ -0,0 +1,16 @@
+class SongOfPi
+
+  PI_VALS = '31415926535897932384626433833'
+
+  def self.valid?(song)
+    song.split(' ').each_with_index do |word, index|
+      return "It's not a pi song." if word.length != PI_VALS[index].to_i
+    end
+    return "It's a pi song."
+  end
+end
+
+t = gets.to_i
+t.times do
+  puts SongOfPi.valid?(gets)
+end
