777207637076d06c5419b57b0a1de2cc2e4e7ea7
Rootul Patel <rootulp@gmail.com>
2015-05-15 00:12:43 -0400

    Solve kaprekar_number
    Used .slice! to get right partition. Left partition is what's leftover
    of num_squared

---
+++ ruby/kaprekar_number.rb
@@ -0,0 +1,15 @@
+class KaprekarNumber
+
+  def self.find_all(min, max)
+    solutions = (min..max).select { |num| valid?(num) }
+    solutions.size == 0 ? 'INVALID RANGE' : solutions.join(' ')
+  end
+
+  def self.valid?(num)
+    num_digits = num.to_s.size
+    num_squared = (num ** 2).to_s
+    num == (num_squared.slice!(0...-num_digits).to_i) + (num_squared.to_i)
+  end
+end
+
+puts KaprekarNumber.find_all(gets.to_i, gets.to_i)
