1def0f8a0dffd02ec45e179f7b3732713ae5cb62
Rootul Patel <rootulp@gmail.com>
2015-05-18 17:08:40 -0400

    quick hackerrank tweets solution

---
+++ ruby/hackerrank_tweets.rb
@@ -0,0 +1,10 @@
+class HackerRankTweets
+  def self.num_mentions(str)
+    str.downcase.scan('hackerrank').size
+  end
+end
+
+t = gets.to_i
+str = ""
+t.times { str << gets }
+puts HackerRankTweets.num_mentions(str)
