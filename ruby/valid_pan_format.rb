488dbaee15ac583e2cd8a33793aaf41986aa31a3
Rootul Patel <rootulp@gmail.com>
2015-05-18 03:12:29 -0400

    quick valid PAN solution

---
+++ ruby/valid_pan_format.rb
@@ -0,0 +1,11 @@
+class PAN
+  PAN_REGEX = /[A-Z]{5}\d{4}[A-Z]{1}/
+  def self.valid?(str)
+    str.match(PAN_REGEX) ? "YES" : "NO"
+  end
+end
+
+t = gets.to_i
+t.times do
+  puts PAN.valid?(gets)
+end
