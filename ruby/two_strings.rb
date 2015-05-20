8dc82d78c0b93c37a2810bc52d734720360351c8
Rootul Patel <rootulp@gmail.com>
2015-05-19 22:43:57 -0400

    quick two strings solution

---
+++ ruby/two_strings.rb
@@ -0,0 +1,19 @@
+class TwoStrings
+
+  def self.valid_substring(str1, str2)
+    substring_helper(str1, str2) ? "YES" : "NO"
+  end
+
+  def self.substring_helper(str1, str2)
+    str2.chars.each { |char| return true if str1.include?(char) }
+    false
+  end
+
+end
+
+t = gets.to_i
+t.times do
+  str1 = gets.chomp
+  str2 = gets.chomp
+  puts TwoStrings.valid_substring(str1, str2)
+end
