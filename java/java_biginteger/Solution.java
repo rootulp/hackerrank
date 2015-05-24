7b8fdc58c8a5685539c868d5005388d62aca5c43
Rootul Patel <rootulp@gmail.com>
2015-05-24 16:38:23 -0400

    solve biginteger

---
+++ java/java_biginteger/Solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    BigInteger a = new BigInteger(sc.next(), 10);
+    BigInteger b = new BigInteger(sc.next(), 10);
+
+    System.out.println(a.add(b));
+    System.out.println(a.multiply(b));
+  }
+}
