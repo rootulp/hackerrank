cd4828353f41f16fc86495cd2b8091464b8f1154
Rootul Patel <rootulp@gmail.com>
2015-05-26 19:08:26 -0400

    solve insert a node at specific position

---
+++ java/linked_lists/insert_a_node_at_a_specific_position_in_a_linked_list.java
@@ -0,0 +1,37 @@
+/*
+  Insert Node at the end of a linked list
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission.
+    // You only need to complete this method.
+
+Node InsertNth(Node head, int data, int position) {
+
+    // Create new node
+    Node temp = new Node();
+    temp.data = data;
+
+
+    // Insert at head
+    if (position == 0) {
+      temp.next = head;
+      return temp;
+    }
+
+    // Iterate through list to find node prior to insert point
+    Node curr = head;
+    for (int i = 0; i < position - 1; i++){
+        curr = curr.next;
+    }
+
+    // Insert node
+    temp.next = curr.next;
+    curr.next = temp;
+
+    return head;
+}
