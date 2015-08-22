db1ee230e7cdb69ef716960ddd23a1e7a760876e
Rootul Patel <rootulp@gmail.com>
2015-08-22 00:46:31 -0400

    solve time conversion

---
+++ ruby/time_conversion.rb
@@ -0,0 +1,17 @@
+class TimeConversion
+
+  def self.convert(time)
+    # Reset noon to midnight
+    hours = time.slice!(0, 2)
+    hours = '00' if hours == '12'
+
+    # Add 12 hours if PM
+    period = time.slice!(-2, 2)
+    period == 'AM' ? "#{hours}#{time}" : "#{hours.to_i + 12}#{time}"
+  end
+
+end
+
+time = gets.chomp
+puts TimeConversion.convert(time)
+
