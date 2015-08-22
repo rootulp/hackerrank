2c9121b915697b5a9a9bb5162381d0c9d2c134cc
Rootul Patel <rootulp@gmail.com>
2015-08-22 00:14:27 -0400

    solve staircase

---
+++ ruby/staircase.rb
@@ -0,0 +1,19 @@
+class Staircase
+
+  def self.draw(height)
+    (1..height).each do |line_num|
+      draw_line(line_num, height)
+    end
+  end
+
+  def self.draw_line(line_num, height)
+    (height - line_num).times { print(' ') }
+    line_num.times { print('#') }
+    print("\n")
+  end
+  private_class_method :draw_line
+
+end
+
+n = gets.chomp.to_i
+Staircase.draw(n)
