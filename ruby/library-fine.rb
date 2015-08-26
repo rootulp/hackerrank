94b05010a6cd061e40b12ea6b6aa47e182169674
Rootul Patel <rootulp@gmail.com>
2015-08-25 23:46:34 -0400

    solve library-fine

---
+++ ruby/library-fine.rb
@@ -0,0 +1,26 @@
+require 'date'
+
+class LibraryFine
+
+  def self.calculate_fine(actual, expected)
+    calculate(Date.new(*parse_date(actual)), Date.new(*parse_date(expected)))
+  end
+
+  def self.parse_date(date)
+    date.split(" ").map(&:to_i).reverse
+  end
+
+  def self.calculate(actual_date, expected_date)
+    return 0     if actual_date <= expected_date
+    return 10000 if actual_date.year != expected_date.year
+
+    actual_date.month != expected_date.month ?
+      500 * (actual_date.month - expected_date.month) :
+      15 * (actual_date.day - expected_date.day)
+  end
+
+  private_class_method :parse_date, :calculate
+
+end
+
+puts LibraryFine.calculate_fine(gets.chomp, gets.chomp)
