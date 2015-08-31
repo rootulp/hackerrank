619016ae148c7f24d62fa606bd5176c11a544ad9
Rootul Patel <rootulp@gmail.com>
2015-08-31 19:04:01 -0400

    solve missing numbers

---
+++ ruby/missing_numbers.rb
@@ -0,0 +1,12 @@
+occurences = Hash.new(0)
+
+_ = gets.to_i
+list_a = gets.split(' ').map(&:to_i)
+_ = gets.to_i
+list_b = gets.split(' ').map(&:to_i)
+
+list_b.each { |num| occurences[num] += 1 }
+list_a.each { |num| occurences[num] -= 1 }
+occurences.keep_if { |num, count| count > 0 }
+
+puts occurences.keys.sort.join(' ')
