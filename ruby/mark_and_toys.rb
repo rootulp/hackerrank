671b31442cd09841182ae15b4287033e16b470e0
Rootul Patel <rootulp@gmail.com>
2015-11-02 15:27:46 -0500

    solve mark and toys

---
+++ ruby/mark_and_toys.rb
@@ -0,0 +1,20 @@
+class MarkAndToys
+
+  attr_reader :total_toys
+  def initialize(money, prices)
+    @total_toys = one_more_toy(money, prices.sort)
+  end
+
+  private
+
+  def one_more_toy(money, prices)
+    money -= prices.shift
+    money <= 0 ? 0 : 1 + one_more_toy(money, prices)
+  end
+
+end
+
+_, money = gets.split(' ').map { |x| x.to_i }
+prices = gets.split(' ').map { |x| x.to_i }
+
+puts MarkAndToys.new(money, prices).total_toys
