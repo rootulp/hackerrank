31fc578a5119faff752c49b99d0c31b867e4688e
Rootul Patel <rootulp@gmail.com>
2015-11-02 17:38:56 -0500

    solve flowers

---
+++ ruby/flowers.rb
@@ -0,0 +1,18 @@
+class Flowers
+
+  attr_reader :k, :total
+  def initialize(k, flowers)
+    @k = k
+    @total = next_c(flowers.sort, 1)
+  end
+
+  def next_c(flowers, c)
+    flowers.any? ? (c * flowers.pop(k).reduce(:+)) + next_c(flowers, c + 1) : 0
+  end
+
+end
+
+_, k = gets.split(' ').map { |x| x.to_i }
+flowers = gets.split(' ').map { |x| x.to_i }
+
+puts Flowers.new(k, flowers).total
