cd41a44e478a9fa6ede89f00c20d39bf4f4257ba
Rootul Patel <rootulp@gmail.com>
2015-11-09 20:14:08 -0500

    solve maxsubarray

---
+++ ruby/maxsubarray.rb
@@ -0,0 +1,25 @@
+class MaxSubArray
+
+  def self.contiguous(arr)
+    return arr.max if all_negative?(arr)
+    total = 0
+    arr.map { |x| total = total + x > 0 ? total + x : 0 }.max
+  end
+
+  def self.non_contiguous(arr)
+    return arr.max if all_negative?(arr)
+    arr.select { |x| x > 0 }.reduce(:+)
+  end
+
+  def self.all_negative?(arr)
+    arr.select { |x| x > 0 }.empty?
+  end
+
+end
+
+t = gets.to_i
+t.times do
+  _ = gets.to_i
+  arr = gets.split(' ').map { |x| x.to_i }
+  puts "#{MaxSubArray.contiguous(arr)} #{MaxSubArray.non_contiguous(arr)}"
+end
