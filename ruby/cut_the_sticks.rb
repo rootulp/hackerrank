b890a81198e522b946bb0e7b5a5d5bc7ba181373
Rootul Patel <rootulp@gmail.com>
2016-04-25 17:59:36 -0400

    cut the sticks: style fix

--- ruby/cut_the_sticks.rb
+++ ruby/cut_the_sticks.rb
@@ -1,14 +1,11 @@
-_ = gets.to_i
-sticks = gets.chomp.split(" ").map { |stick| stick.to_i }
-
-def main(sticks)
-  if !sticks.empty?
-    puts sticks.length
-    cut_size = sticks.min
-    sticks.map! { |stick| stick - cut_size }
-    sticks.delete(0)
-    main(sticks)
-  end
+def cut_sticks(sticks)
+  return if sticks.empty?
+  puts sticks.length
+  cut_size = sticks.min
+  sticks.map! { |stick| stick - cut_size }
+  sticks.delete(0)
+  cut_sticks(sticks)
 end
 
-main(sticks)
+_ = gets.to_i
+cut_sticks(gets.chomp.split(' ').map(&:to_i))
