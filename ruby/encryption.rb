3c4a1618f20574a6e68b8f8ed498a3b9ce65d2d4
Rootul Patel <rootulp@gmail.com>
2016-04-26 15:20:52 -0400

    encryption: style fix

--- ruby/encryption.rb
+++ ruby/encryption.rb
@@ -1,10 +1,8 @@
 rectangle = []
-result = ""
-
-phrase = gets.gsub(/\s+/, "").split(//)
+result = ''
+phrase = gets.gsub(/\s+/, '').split(//)
 cols = (phrase.length**0.5).ceil
-phrase.each_slice(cols) {|row| rectangle << row}
+phrase.each_slice(cols) { |row| rectangle << row }
 encoded = rectangle.first.zip(*rectangle.drop(1))
-encoded.each {|col| result << col.join() + " "}
-
+encoded.each { |col| result << col.join + ' ' }
 puts result
