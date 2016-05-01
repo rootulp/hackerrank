f0b4183a4849c5c00e0f597e7619402bf06ace35
Rootul Patel <rootulp@gmail.com>
2016-05-01 17:55:41 -0400

    filling-jars: style fix

--- ruby/filling_jars.rb
+++ ruby/filling_jars.rb
@@ -1,7 +1,7 @@
-(size, operations) = gets.split.map{ |i| i.to_i }
+(size, operations) = gets.split.map(&:to_i)
 total = 0
 operations.times do
-  (start, stop, value) = gets.split.map{ |i| i.to_i }
+  (start, stop, value) = gets.split.map(&:to_i)
   num = stop - start + 1
   total += num * value
 end
