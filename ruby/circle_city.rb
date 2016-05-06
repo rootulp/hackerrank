18968837a0db10c571d3bc9a82a15dbf09c381b2
Rootul Patel <rootulp@gmail.com>
2016-05-06 16:12:22 -0400

    Circle-city: style fix
    Solution still times out

--- ruby/circle_city.rb
+++ ruby/circle_city.rb
@@ -1,23 +1,16 @@
 def possible?(radius_squared, stations)
-    cities_needed = 4
-    radius = (radius_squared ** 0.5).to_i
-    for x in 1..(radius-1)
-        for y in 1..(radius-1)
-            if x**2 + y**2 == radius_squared
-                cities_needed += 4
-            end
-        end
+  cities_needed = 4
+  radius = (radius_squared**0.5).to_i
+  (1..(radius - 1)).each do |x|
+    (1..(radius - 1)).each do |y|
+      cities_needed += 4 if x**2 + y**2 == radius_squared
     end
-    return true if stations >= cities_needed
-    false
+  end
+  stations >= cities_needed
 end
 
 t = gets.to_i
 t.times do
-    (radius_squared, stations) = gets.split(" ").map{|x| x.to_i}
-    if possible?(radius_squared, stations)
-        puts "possible"
-    else
-        puts "impossible"
-    end
+  (radius_squared, stations) = gets.split(' ').map(&:to_i)
+  puts possible?(radius_squared, stations) ? 'possible' : 'impossible'
 end
