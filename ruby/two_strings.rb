d3de020c8e5dfc8cd4bb6e9b0d3caaaad7db4e0a
Rootul Patel <rootulp@gmail.com>
2016-05-08 17:44:56 -0400

    Two-strings: style fix

--- ruby/two_strings.rb
+++ ruby/two_strings.rb
@@ -1,14 +1,13 @@
+# Two Strings
 class TwoStrings
-
   def self.valid_substring(str1, str2)
-    substring_helper(str1, str2) ? "YES" : "NO"
+    substring_helper(str1, str2) ? 'YES' : 'NO'
   end
 
   def self.substring_helper(str1, str2)
     str2.chars.each { |char| return true if str1.include?(char) }
     false
   end
-
 end
 
 t = gets.to_i
