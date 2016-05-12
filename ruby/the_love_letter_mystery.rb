8251df7203e9aafa5c66109cc78ee86f11eb836a
Rootul Patel <rootulp@gmail.com>
2016-05-12 12:27:34 -0400

    The-love-letter-mystery: slight refactor

--- ruby/the_love_letter_mystery.rb
+++ ruby/the_love_letter_mystery.rb
@@ -1,30 +1,22 @@
-n = gets.to_i
-test_cases = []
-n.times {|x| test_cases.push(gets.chomp)}
-
 def analyze(test_arr, num_reductions)
-    if test_arr.length <= 1
-        return num_reductions
-    else
-        x = test_arr.pop
-        y = test_arr.shift
-        num_reductions += num_reductions_for_pair(x, y, 0)
-        return analyze(test_arr, num_reductions)
-    end
+  return num_reductions if test_arr.length <= 1
+  x = test_arr.pop
+  y = test_arr.shift
+  num_reductions += num_reductions_for_pair(x, y, 0)
+  analyze(test_arr, num_reductions)
 end
 
 def num_reductions_for_pair(x, y, counter)
-    comparison = x <=> y
-    if  comparison == 1
-        return num_reductions_for_pair((x.ord - 1).chr, y, counter + 1)
-    elsif comparison == -1
-        return num_reductions_for_pair(x, (y.ord - 1).chr, counter + 1)
-    else
-        return counter
-    end
+  comparison = x <=> y
+  if comparison == 1
+    num_reductions_for_pair((x.ord - 1).chr, y, counter + 1)
+  elsif comparison == -1
+    num_reductions_for_pair(x, (y.ord - 1).chr, counter + 1)
+  else
+    counter
+  end
 end
 
-test_cases.each do |test_str|
-    test_arr = test_str.split(//)
-    puts analyze(test_arr, 0)
+gets.to_i.times do
+  puts analyze(gets.chomp.chars, 0)
 end
