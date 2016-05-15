a1f178098ff8d11e89e1a7ed021c5a05610033a2
Rootul Patel <rootulp@gmail.com>
2016-05-15 19:53:18 -0400

    Song-of-pi: style fix

--- ruby/song_of_pi.rb
+++ ruby/song_of_pi.rb
@@ -1,12 +1,12 @@
+# Song of Pi
 class SongOfPi
-
-  PI_VALS = '31415926535897932384626433833'
+  PI_VALS = '31415926535897932384626433833'.freeze
 
   def self.valid?(song)
     song.split(' ').each_with_index do |word, index|
       return "It's not a pi song." if word.length != PI_VALS[index].to_i
     end
-    return "It's a pi song."
+    "It's a pi song."
   end
 end
 
