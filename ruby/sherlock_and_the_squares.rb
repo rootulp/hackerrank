75cebe5c5aaebefbc37016c4bf4c8894c236a0ed
Rootul Patel <rootulp@gmail.com>
2016-05-24 16:39:21 -0400

    Sherlock-and-the-squares: style fix

--- ruby/sherlock_and_the_squares.rb
+++ ruby/sherlock_and_the_squares.rb
@@ -1,7 +1,7 @@
 t = gets.to_i
-t.times{
-  (a, b) = gets.split.map{|i| i.to_i}
-  small_a = ((a-1)**0.5).floor
+t.times do
+  (a, b) = gets.split.map(&:to_i)
+  small_a = ((a - 1)**0.5).floor
   small_b = (b**0.5).floor
   puts small_b - small_a
-}
+end
