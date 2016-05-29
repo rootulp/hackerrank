e1250fbcae6ab2ee8dea60fa27d997f769def0b3
Rootul Patel <rootulp@gmail.com>
2016-05-29 18:12:32 -0400

    Restaurant: style fix

--- ruby/restaurant.rb
+++ ruby/restaurant.rb
@@ -1,6 +1,6 @@
 t = gets.to_i
 t.times do
-  (length, width) = gets.split(" ").map{|x| x.to_i}
+  (length, width) = gets.split(' ').map(&:to_i)
   side = length.gcd(width)
   puts (length * width) / (side**2)
 end
