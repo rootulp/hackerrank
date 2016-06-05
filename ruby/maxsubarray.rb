855d4856a98805d1b3bb4b7b0e5dbe76662e390c
Rootul Patel <rootulp@gmail.com>
2016-06-04 22:54:41 -0400

    Max-subarray: style fix

--- ruby/maxsubarray.rb
+++ ruby/maxsubarray.rb
@@ -1,5 +1,5 @@
+# Max Subarray
 class MaxSubArray
-
   def self.contiguous(arr)
     return arr.max if all_negative?(arr)
     total = 0
@@ -14,12 +14,11 @@ class MaxSubArray
   def self.all_negative?(arr)
     arr.select { |x| x > 0 }.empty?
   end
-
 end
 
 t = gets.to_i
 t.times do
   _ = gets.to_i
-  arr = gets.split(' ').map { |x| x.to_i }
+  arr = gets.split(' ').map(&:to_i)
   puts "#{MaxSubArray.contiguous(arr)} #{MaxSubArray.non_contiguous(arr)}"
 end
