0e792bf62109ace2e8a1c5ad46546094a6bb2ea7
Rootul Patel <rootulp@gmail.com>
2016-06-07 01:18:46 -0400

    Manasa-and-stones: style fix

--- ruby/manasa_and_stones.rb
+++ ruby/manasa_and_stones.rb
@@ -1,16 +1,20 @@
 def last_stone(stones, a, b)
-    results = []
-    for i in 0..stones do
-        results << (i*a) + ((stones-i)*b)
-        results << (i*b) + ((stones-i)*a)
-    end
-    puts results.uniq.sort.join(" ")
+  puts all_stones(stones, a, b).uniq.sort.join(' ')
+end
+
+def all_stones(stones, a, b)
+  results = []
+  (0..stones).each do |i|
+    results << (i * a) + ((stones - i) * b)
+    results << (i * b) + ((stones - i) * a)
+  end
+  results
 end
 
 t = gets.to_i
-t.times{
-    stones = gets.to_i
-    a = gets.to_i
-    b = gets.to_i
-    last_stone(stones-1, a, b)
-}
+t.times do
+  stones = gets.to_i
+  a = gets.to_i
+  b = gets.to_i
+  last_stone(stones - 1, a, b)
+end
