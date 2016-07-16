8b432349010b17d55fecfe4389fb8c33dee3e2bd
Rootul Patel <rootulp@gmail.com>
2016-07-15 20:51:12 -0400

    Pairs: refactor

--- ruby/pairs.rb
+++ ruby/pairs.rb
@@ -1,30 +1,53 @@
 # Pairs
 class Pairs
-  attr_reader :num_pairs
-  def initialize(k, arr)
-    @num_pairs = calculate_num_pairs(k, arr.sort)
+  attr_reader :k, :integers, :left, :right, :pairs
+  def initialize(k, integers)
+    @k = k
+    @integers = integers.sort
+    @left = 0
+    @right = 0
+    @pairs = 0
+    calculate_pairs
   end
 
   private
 
-  def calculate_num_pairs(k, arr)
-    pairs = 0
-    left = 0
-    right = 0
-
-    loop do
-      return pairs if left == arr.size - 1
-
-      if left == right || arr[right] - arr[left] < k
-        right == arr.size - 1 ? left += 1 : right += 1
-      else
-        pairs += 1 if arr[right] - arr[left] == k
-        left += 1
-      end
+  def calculate_pairs
+    until reached_end?(left)
+      increment_pairs if difference_equals_k?(integers[left], integers[right])
+      move_pointers
     end
+  end
+
+  def move_pointers
+    if integers[right] - integers[left] < k && !reached_end?(right)
+      increment_right
+    else
+      increment_left
+    end
+  end
+
+  def difference_equals_k?(int1, int2)
+    int2 - int1 == k
+  end
+
+  def reached_end?(index)
+    index == integers.size - 1
+  end
+
+  def increment_left
+    @left += 1
+  end
+
+  def increment_right
+    @right += 1
+  end
+
+  def increment_pairs
+    @pairs += 1
   end
 end
 
 _, k = gets.split(' ').map(&:to_i)
-arr = gets.split(' ').map(&:to_i)
-puts Pairs.new(k, arr).num_pairs
+integers = gets.split(' ').map(&:to_i)
+puts Pairs.new(k, integers).pairs
