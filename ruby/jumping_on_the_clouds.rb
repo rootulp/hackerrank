cf79505fc1044043d329d51290dcbbf7e01614fe
Rootul Patel <rootulp@gmail.com>
2016-07-23 20:07:25 -0400

    Jumping-on-the-clouds: solve

---
+++ ruby/jumping_on_the_clouds.rb
@@ -0,0 +1,11 @@
+_ = gets.chomp
+clouds = gets.chomp.split(' ').map(&:to_i)
+
+current_cloud = 0
+target_cloud = clouds.size - 1
+jumps = 0
+until current_cloud == target_cloud
+  current_cloud += clouds[current_cloud + 2] == 0 ? 2 : 1
+  jumps += 1
+end
+puts jumps
