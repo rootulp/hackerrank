3feadad1d79f0aaddf4698a79778dfed1c263478
Rootul Patel <rootulp@palantir.com>
2017-09-08 22:32:50 -0700

    Solve making-anagrams w/ Counter

---
+++ python/ctci-making-anagrams.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+
+def number_needed(a, b):
+    counts = Counter(a)
+    counts.subtract(Counter(b))
+    return sum(map(abs, counts.values()))
+
+a = input().strip()
+b = input().strip()
+
+print(number_needed(a, b))
