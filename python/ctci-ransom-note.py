47dd6e0bf62e5ff1d8a523aaf1a127b10594ab38
Rootul Patel <rootulp@palantir.com>
2017-09-09 14:36:25 -0700

    Start Ransom Note

---
+++ python/ctci-ransom-note.py
@@ -0,0 +1,12 @@
+def ransom_note(magazine, ransom):
+    
+
+m, n = map(int, input().strip().split(' '))
+magazine = input().strip().split(' ')
+ransom = input().strip().split(' ')
+answer = ransom_note(magazine, ransom)
+if(answer):
+    print("Yes")
+else:
+    print("No")
+    
