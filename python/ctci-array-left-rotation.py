cf1ccfda11c082ca0cbae0356f35092f09df542a
Rootul Patel <rootulp@palantir.com>
2017-09-08 22:10:47 -0700

    Solve array-left-rotation w/ deque

---
+++ python/ctci-array-left-rotation.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+def array_left_rotation(a, n, k):
+    queue = deque(a)
+    for x in range(k):
+        queue.append(queue.popleft())
+    return list(queue)
+
+n, k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+answer = array_left_rotation(a, n, k);
+print(*answer, sep=' ')
