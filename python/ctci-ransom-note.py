c731260d8efc4484e4deaa51d1b89ae4cd2f70c5
Rootul Patel <rootulp@palantir.com>
2017-09-09 15:17:59 -0700

    Solve Ransom Note

--- python/ctci-ransom-note.py
+++ python/ctci-ransom-note.py
@@ -1,5 +1,14 @@
-def ransom_note(magazine, ransom):
-    
+from collections import Counter
+
+
+def ransom_note(magazine_words, ransom_words):
+    available_words = Counter(magazine_words)
+    needed_words = Counter(ransom_words)
+    return contains(available_words, needed_words)
+
+
+def contains(larger, smaller):
+    return not smaller - larger
 
 m, n = map(int, input().strip().split(' '))
 magazine = input().strip().split(' ')
@@ -9,4 +18,3 @@ if(answer):
     print("Yes")
 else:
     print("No")
-    
