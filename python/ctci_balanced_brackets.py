538c732598360fa42c2d513f0686b81c66a7e816
Rootul Patel <rootulp@palantir.com>
2017-09-09 20:38:56 -0700

    Solve Balanced Brackets

--- python/ctci_balanced_brackets.py
+++ python/ctci_balanced_brackets.py
@@ -1,10 +1,26 @@
-def is_matched(expression):
-    pass
+class BalancedBrackets(object):
+    
+    BRACKETS = {
+        "{": "}",
+        "[": "]",
+        "(": ")"
+    }
+    
+    @classmethod
+    def is_balanced(cls, expression):
+        stack = []
+        for char in expression:
+            if char in cls.BRACKETS:
+                stack.append(char)
+            elif not stack or cls.BRACKETS.get(stack.pop()) != char:
+                return False
+        return not stack
 
-t = int(input().strip())
-for a0 in range(t):
+
+NUM_CASES = int(input().strip())
+for _ in range(NUM_CASES):
     expression = input().strip()
-    if is_matched(expression) == True:
+    if BalancedBrackets.is_balanced(expression):
         print("YES")
     else:
         print("NO")
