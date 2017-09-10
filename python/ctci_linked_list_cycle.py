1c74509445ebd7941fbeebd6166085a3c18122e9
Rootul Patel <rootulp@palantir.com>
2017-09-10 00:18:07 -0700

    Start linked list cycle

---
+++ python/ctci_linked_list_cycle.py
@@ -0,0 +1,14 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+
+def has_cycle(head):
+    pass
