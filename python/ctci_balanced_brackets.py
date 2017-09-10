a5b18e3cb7b8816d15126ead5992a40f2a026335
Rootul Patel <rootulp@palantir.com>
2017-09-09 20:15:22 -0700

    Start Balanced Brackets

---
+++ python/ctci_balanced_brackets.py
@@ -0,0 +1,10 @@
+def is_matched(expression):
+    pass
+
+t = int(input().strip())
+for a0 in range(t):
+    expression = input().strip()
+    if is_matched(expression) == True:
+        print("YES")
+    else:
+        print("NO")
