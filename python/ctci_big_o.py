2971dfb2ace720fea55fb851f10d94ebd180a80a
Rootul Patel <rootulp@palantir.com>
2017-09-10 01:47:59 -0700

    Should have tried naive solution first

--- python/ctci_big_o.py
+++ python/ctci_big_o.py
@@ -1,39 +1,19 @@
-from collections import deque
+import math
 
 
-class Sieve(object):
-
-    def __init__(self, upper_bound):
-        self.upper_bound = upper_bound + 1
-        self.primes = []
-        self.populate_primes()
-        # print("Primes " + str(self.primes))
-
-    def is_prime(self, potential_prime):
-        return potential_prime in self.primes
-
-    def populate_primes(self,):
-        remaining = deque(range(2, self.upper_bound))
-        while remaining:
-            prime = remaining.popleft()
-            self.primes.append(prime)
-            for multiple in self.multiples(prime):
-                if multiple in remaining:
-                    remaining.remove(multiple)
-
-    def multiples(self, num):
-        return range(num, self.upper_bound, num)
+def is_prime(test_case):
+    if test_case == 1:
+        return False
+    for factor in range(2, math.ceil(math.sqrt(test_case) + 1)):
+        if test_case != factor and test_case % factor == 0:
+            return False
+    return True
 
 
 NUM_CASES = int(input().strip())
-TEST_CASES = []
 for _ in range(NUM_CASES):
-    TEST_CASES.append(int(input().strip()))
-
-# print("Max: " + str(max(TEST_CASES)))
-SIEVE = Sieve(max(TEST_CASES))
-for test_case in TEST_CASES:
-    if SIEVE.is_prime(test_case):
+    test_case = int(input().strip())
+    if is_prime(test_case):
         print("Prime")
     else:
         print("Not prime")
