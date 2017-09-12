f79e8b3cee0221be9f092d2a7712a06ce0b07a7a
Rootul Patel <rootulp@palantir.com>
2017-09-11 20:35:32 -0700

    Solve Ice Cream Parlor

---
+++ java/ctci_ice_cream_parlor.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int trips = in.nextInt();
+        for(int trip_i = 0; trip_i < trips; trip_i++) {
+            int money = in.nextInt();
+            int flavors = in.nextInt();
+            int costs[] = new int[flavors];
+            for(int cost_i = 0; cost_i < flavors; cost_i++) {
+                costs[cost_i] = in.nextInt();
+            }
+            for(int i = 0; i < flavors - 1; i++) {
+                for(int j = i + 1; j < flavors; j++) {
+                    if (costs[i] + costs[j] == money) {
+                        System.out.print(i + 1);
+                        System.out.print(" ");
+                        System.out.println(j + 1);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
