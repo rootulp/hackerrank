43057480dc26d049ada175b215cdb2bfc9e0c6ff
Rootul Patel <rootulp@palantir.com>
2017-09-13 21:44:18 -0700

    Solve lonely integer

---
+++ python/ctci_lonely_integer.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import sys
+
+def lonely_integer(a):
+    result = 0
+    for val in a:
+        result ^= val
+    return result
+
+
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+print(lonely_integer(a))
