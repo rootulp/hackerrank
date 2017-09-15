93536f18d6d5094d4c0eb0553ff686a4be115b90
Rootul Patel <rootulp@palantir.com>
2017-09-14 22:29:24 -0700

    Solve recursive staircase

--- python/ctci_recursive_staircase.py
+++ python/ctci_recursive_staircase.py
@@ -1,14 +1,19 @@
-def staircase(num_stairs):
-    if num_stairs == 1:
-        return 1
-    elif num_stairs == 2:
-        return 2
-    elif num_stairs == 3:
-        return 4
-    else:
-        return staircase(num_stairs - 1) + staircase(num_stairs - 2) + staircase(num_stairs - 3)
+cache = {
+    1: 1,
+    2: 2,
+    3: 4
+}
 
-s = int(input().strip())
-for a0 in range(s):
-    n = int(input().strip())
-    print(staircase(n))
+
+def staircase(num_stairs):
+    if num_stairs not in cache:
+        cache[num_stairs] = (staircase(num_stairs - 1) +
+                             staircase(num_stairs - 2) +
+                             staircase(num_stairs - 3))
+    return cache[num_stairs]
+
+
+test_cases = int(input().strip())
+for _ in range(test_cases):
+    num_stairs = int(input().strip())
+    print(staircase(num_stairs))
