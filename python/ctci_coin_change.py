933f4ba9e3a815c73fb38adea31520bcc25a2fa6
Rootul Patel <rootulp@palantir.com>
2017-09-18 22:34:25 -0400

    Uncommited coin change improvements

--- python/ctci_coin_change.py
+++ python/ctci_coin_change.py
@@ -1,38 +1,46 @@
 #!/bin/python3
 
-import sys
 import copy
+import operator
 from collections import Counter
+from collections import defaultdict
+from functools import reduce
 
-class hashabledict(dict):
-  def __key(self):
-    return tuple((k,self[k]) for k in sorted(self))
-  def __hash__(self):
-    return hash(self.__key())
-  def __eq__(self, other):
-    return self.__key() == other.__key()
+def prod(iterable):
+    return reduce(operator.mul, iterable, 1)
+
+class HashableDict(dict):
+    def __key(self):
+        return tuple((k, self[k]) for k in sorted(self))
+    def __hash__(self):
+        return hash(self.__key())
+    def __eq__(self, other):
+        return self.__key() == other.__key()
 
 class CoinChange(object):
 
     def __init__(self, available_coins, n):
         self.available_coins = available_coins
         self.n = n
-        self.solutions = set()
+        self.solutions = defaultdict(lambda: set())
 
-    def make_change(self, n, coins_used = Counter()):
-        if n < 0:
-            return
-        elif n == 0:
-            self.solutions.add(hashabledict(coins_used))
+    def make_change(self, dollars, coins_used = Counter()):
+        current = sum(list(map(prod, dict(coins_used).items())))
+        if current >= dollars:
+            return;
+        elif current != 0 and HashableDict(coins_used) not in self.solutions[current]:
+            self.solutions[current].add(HashableDict(coins_used))
+            return coins_used
         else:
             for coin in self.available_coins:
                 coins = copy.deepcopy(coins_used)
                 coins[coin] += 1
-                self.make_change(n - coin, coins)
+                coins_used_in_here = self.make_change(dollars, coins)
+                self.solutions[current].add(HashableDict(coins_used_in_here))
 
 
 n, m = map(int, input().strip().split(' '))
 available_coins = [int(coins_temp) for coins_temp in input().strip().split(' ')]
 coin_change = CoinChange(available_coins, n)
 coin_change.make_change(n)
-print(len(coin_change.solutions))
+print(coin_change.solutions)
