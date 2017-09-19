a909aa209a6072065a2b8ac3ae18d0a9751c7ade
Rootul Patel <rootulp@palantir.com>
2017-09-19 10:58:45 -0400

    solve quicksort1

---
+++ python/quicksort1.py
@@ -0,0 +1,14 @@
+def divide(arr):
+    pivot = arr[0]
+    left = []
+    right = []
+    for i in arr:
+        if i < pivot:
+            left.append(i)
+        elif i > pivot:
+            right.append(i)
+        return left + [pivot] + right
+
+n = int(input().strip())
+arr = list(map(int, input().strip().split(" ")))
+print(" ".join(map(str, divide(arr))))
