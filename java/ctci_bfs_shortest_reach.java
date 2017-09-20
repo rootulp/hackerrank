9ac29128661ee2beaa29859bc422c72662fe9549
Rootul Patel <rootulp@palantir.com>
2017-09-20 01:14:32 -0400

    camelCase everything!

--- java/ctci_bfs_shortest_reach.java
+++ java/ctci_bfs_shortest_reach.java
@@ -5,43 +5,42 @@ public class Solution {
 
     private int nodes;
     private int edges;
-    private LinkedList<Integer>[] adjacency_lists;
+    private LinkedList<Integer>[] adjacencyLists;
 
     public Solution(int nodes, int edges) {
         this.nodes = nodes;
         this.edges = edges;
-        this.adjacency_lists = new LinkedList[nodes];
+        this.adjacencyLists = new LinkedList[nodes];
         for (int i = 0; i < nodes; i++) {
-            adjacency_lists[i] = new LinkedList<Integer>();
+            adjacencyLists[i] = new LinkedList<Integer>();
         }
     }
 
-    public void add_edge(int node_a, int node_b) {
-        if (!this.adjacency_lists[node_a].contains(node_b)) {
-            this.adjacency_lists[node_a].add(node_b);
+    public void add_edge(int nodeA, int nodeB) {
+        if (!this.adjacencyLists[nodeA].contains(nodeB)) {
+            this.adjacencyLists[nodeA].add(nodeB);
         }
-        if (!this.adjacency_lists[node_b].contains(node_a)) {
-            this.adjacency_lists[node_b].add(node_a);
+        if (!this.adjacencyLists[nodeB].contains(nodeA)) {
+            this.adjacencyLists[nodeB].add(nodeA);
         }
     }
 
-    public String distances_from(int start_node) {
-        Map<Integer, Integer> visited = breadth_first_search(start_node);
-
-        List<Integer> distances_from = new ArrayList<Integer>();
+    public String distancesFrom(int startNode) {
+        Map<Integer, Integer> visited = breadthFirstSearch(startNode);
+        List<Integer> distancesFrom = new ArrayList<Integer>();
         for (int i = 0; i < this.nodes; i++) {
-            if (i == start_node) {
+            if (i == startNode) {
                 continue;
             } else if (visited.containsKey(i)) {
-                distances_from.add(visited.get(i) * 6);
+                distancesFrom.add(visited.get(i) * 6);
             } else {
-                distances_from.add(-1);
+                distancesFrom.add(-1);
             }
         }
-        return space_delimit(distances_from);
+        return spaceDelimit(distancesFrom);
     }
 
-    public String space_delimit(List<Integer> list) {
+    public String spaceDelimit(List<Integer> list) {
         StringJoiner joiner = new StringJoiner(" ");
         for (Integer i : list) {
             joiner.add(i.toString());
@@ -49,7 +48,7 @@ public class Solution {
         return joiner.toString();
     }
 
-    private Map<Integer, Integer> breadth_first_search(int startNode) {
+    private Map<Integer, Integer> breadthFirstSearch(int startNode) {
         Map<Integer, Integer> visited = new HashMap<Integer, Integer>();
         Queue<Integer> nodesToVisit = new LinkedList<Integer>();
         Queue<Integer> nodesToVisitDepth = new LinkedList<Integer>();
@@ -60,35 +59,33 @@ public class Solution {
         while (nodesToVisit.size() != 0) {
             int currentNode = nodesToVisit.poll();
             int currentDepth = nodesToVisitDepth.poll();
-            for (int neighbor : this.adjacency_lists[currentNode]){
+            for (int neighbor : this.adjacencyLists[currentNode]){
                 if (!visited.containsKey(neighbor)) {
                     nodesToVisit.add(neighbor);
                     nodesToVisitDepth.add(currentDepth + 1);
                 }
             }
             if (!visited.containsKey(currentNode) || visited.get(currentNode) > currentDepth) {
-                //System.out.println("Putting: " + Integer.toString(currentNode) + " Depth: " + Integer.toString(currentDepth));
                 visited.put(currentNode, currentDepth);
             }
         }
-        //System.out.println(visited);
         return visited;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int queries = in.nextInt();
-        for (int query_i = 0; query_i < queries; query_i++) {
+        for (int query = 0; query < queries; query++) {
             int nodes = in.nextInt();
             int edges = in.nextInt();
             Solution graph = new Solution(nodes, edges);
-            for (int edge_i = 0; edge_i < edges; edge_i++) {
-                int node_a = in.nextInt();
-                int node_b = in.nextInt();
-                graph.add_edge(node_a - 1, node_b - 1);
+            for (int edge = 0; edge < edges; edge++) {
+                int nodeA = in.nextInt();
+                int nodeB = in.nextInt();
+                graph.add_edge(nodeA - 1, nodeB - 1);
             }
-            int start_node = in.nextInt();
-            System.out.println(graph.distances_from(start_node - 1));
+            int startNode = in.nextInt();
+            System.out.println(graph.distancesFrom(startNode - 1));
         }
     }
 }
