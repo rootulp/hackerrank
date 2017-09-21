bb4c033bb18e83dd1c7352d4d4952e54b407c880
Rootul Patel <rootulp@palantir.com>
2017-09-21 15:08:52 -0400

    Style fixes

--- python/connected_cell_in_a_grid.py
+++ python/connected_cell_in_a_grid.py
@@ -13,7 +13,9 @@ class Grid(object):
     def region_size(self, row, col):
         if not self.filled_cell(row, col):
             return 0
+        return self.bfs_for_region_size(row, col)
 
+    def bfs_for_region_size(self, row, col):
         visited = set()
         to_visit = [(row, col)]
         while to_visit:
@@ -21,21 +23,17 @@ class Grid(object):
             if (row, col) not in visited:
                 visited.add((row, col))
                 to_visit.extend(self.filled_neighbors(row, col))
-
         return len(visited)
 
     def filled_neighbors(self, row, col):
-        filled_neighbors = list(filter(lambda cell: self.filled_cell(cell[0], cell[1]), self.neighbors(row, col)))
-        # print("filled_neighbors: " + str(filled_neighbors))
-        return filled_neighbors
+        return list(filter(lambda cell: self.filled_cell(cell[0], cell[1]),
+                           self.neighbors(row, col)))
 
     def neighbors(self, row, col):
-        neighbors = list(filter(lambda cell:
+        return list(filter(lambda cell:
                            self.row_in_grid(cell[0]) and
                            self.col_in_grid(cell[1]),
                            self.potential_neighbors(row, col)))
-        # print("neighbors: " + str(neighbors))
-        return neighbors
 
     def row_in_grid(self, row):
         return 0 <= row < self.rows
@@ -45,14 +43,14 @@ class Grid(object):
 
     def potential_neighbors(self, row, col):
         return [(row + row_delta, col + col_delta)
-                for row_delta in range (-1, 2)
-                for col_delta in range (-1, 2)]
+                for row_delta in range(-1, 2)
+                for col_delta in range(-1, 2)]
 
     def filled_cell(self, row, col):
         return self.grid[row][col] == 1
 
+
 rows = int(input().strip())
 cols = int(input().strip())
 grid = [list(map(int, input().strip().split(" "))) for _ in range(rows)]
-g = Grid(grid)
-print(g.largest_region())
+print(Grid(grid).largest_region())
