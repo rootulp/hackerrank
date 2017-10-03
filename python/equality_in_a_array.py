059f084da18bf5e27aa0369bc4658dbccc6789a4
Rootul Patel <rootulp@palantir.com>
2017-10-03 23:33:57 +0100

    Solve equality in a array

---
+++ python/equality_in_a_array.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+size = int(input().strip())
+counts = Counter((map(int, input().strip().split(' '))))
+element, count = counts.most_common(1)[0]
+print (size - count)
