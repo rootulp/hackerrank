4516f8c65a75f945ba00ec98235d5ee6d8bda109
Rootul Patel <rootulp@palantir.com>
2017-10-16 20:31:02 +0100

    Use house_location range

--- python/apple_and_orange.py
+++ python/apple_and_orange.py
@@ -4,9 +4,10 @@ import sys
 
 
 house_left, house_right = map(int, input().strip().split(' '))
+house_location = range(house_left, house_right + 1)
 apple_tree, orange_tree = map(int, input().strip().split(' '))
 _, _ = map(int, input().strip().split(' '))
 apple_distances = map(int, input().strip().split(' '))
 orange_distances = map(int, input().strip().split(' '))
-print(len(list((filter(lambda apple_distance: house_left <= apple_tree + apple_distance <= house_right, apple_distances)))))
-print(len(list(filter(lambda orange_distance: house_left <= orange_tree + orange_distance <= house_right, orange_distances))))
+print(len(list((filter(lambda apple_distance: apple_tree + apple_distance in house_location, apple_distances)))))
+print(len(list(filter(lambda orange_distance: orange_tree + orange_distance in house_location, orange_distances))))
