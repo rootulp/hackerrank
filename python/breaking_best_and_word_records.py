de97c5565f3ae3548750e083c16a2362c9e8b3ee
Rootul Patel <rootulp@palantir.com>
2017-10-17 21:09:19 +0100

    Solve breaking the records

---
+++ python/breaking_best_and_word_records.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+def getRecord(scores):
+    highest_score = scores[0]
+    lowest_score = scores[0]
+    times_score_increased = 0
+    times_score_decreased = 0
+    for score in scores:
+        if score > highest_score:
+            highest_score = score
+            times_score_increased += 1
+        elif score < lowest_score:
+            lowest_score = score
+            times_score_decreased += 1
+    return times_score_increased, times_score_decreased
+
+_ = int(input().strip())
+SCORES = list(map(int, input().strip().split(' ')))
+print (" ".join(map(str, getRecord(SCORES))))
