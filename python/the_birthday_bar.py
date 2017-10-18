5a39e25c56a4d32f51c16a4a94290750d26daf0b
Rootul Patel <rootulp@palantir.com>
2017-10-18 20:13:03 +0100

    Solve birthday chocolate

---
+++ python/the_birthday_bar.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import itertools
+import collections
+
+def sliding_window(n, seq):
+    """
+    Copied from toolz
+    https://toolz.readthedocs.io/en/latest/_modules/toolz/itertoolz.html#sliding_window
+
+    A sequence of overlapping subsequences
+
+    >>> list(sliding_window(2, [1, 2, 3, 4]))
+    [(1, 2), (2, 3), (3, 4)]
+
+    This function creates a sliding window suitable for transformations like
+    sliding means / smoothing
+
+    >>> mean = lambda seq: float(sum(seq)) / len(seq)
+    >>> list(map(mean, sliding_window(2, [1, 2, 3, 4])))
+    [1.5, 2.5, 3.5]
+    """
+    return zip(*(collections.deque(itertools.islice(it, i), 0) or it
+               for i, it in enumerate(itertools.tee(seq, n))))
+
+def birthday_chocolate(squares, day, month):
+    birthday_chocolates = 0
+    for piece in sliding_window(month, squares):
+        if sum(piece) == day:
+            birthday_chocolates += 1
+    return birthday_chocolates
+
+_ = int(input().strip())
+SQUARES = list(map(int, input().strip().split(' ')))
+DAY, MONTH = map(int, input().strip().split(' '))
+print(birthday_chocolate(SQUARES, DAY, MONTH))
