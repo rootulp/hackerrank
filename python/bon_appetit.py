e9fc924053d6114917d422bcd2cb32c7a66edaa3
Rootul Patel <rootulp@palantir.com>
2017-10-24 23:17:30 +0100

    Solve Bon Appetit

---
+++ python/bon_appetit.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+def bon_appetit(costs, item_anna_skipped, brian_charged_anna):
+    annas_share = cost_of_annas_share(costs, item_anna_skipped)
+    return "Bon Appetit" if brian_charged_anna == annas_share else brian_charged_anna - annas_share
+
+def cost_of_annas_share(costs, item_anna_skipped):
+    return int(sum(costs[:item_anna_skipped] + costs[item_anna_skipped + 1:]) / 2)
+
+
+_, item_anna_skipped = list(map(int, input().strip().split(' ')))
+costs = list(map(int, input().strip().split(' ')))
+brian_charged_anna = int(input().strip())
+print(bon_appetit(costs, item_anna_skipped, brian_charged_anna))
