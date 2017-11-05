2e2d9f2ffff4855ecea0007148d808bd04c28f2c
Rootul Patel <rootulp@palantir.com>
2017-11-05 18:35:09 +0000

    Solve Project Euler #6: Sum square difference

---
+++ python/euler006.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+def square_of_sum(n):
+    return sum(range(n + 1)) ** 2
+
+def sum_of_squares(n):
+    return int(n * (n + 1) * (2 * n + 1) / 6)
+
+def sum_square_diff(n):
+    return square_of_sum(n) - sum_of_squares(n)
+
+test_cases = int(input().strip())
+for _ in range(test_cases):
+    n = int(input().strip())
+    print(sum_square_diff(n))
+
