bb5787c769c50aa0b1c7d810a92a20fa70cd2411
Rootul Patel <rootulp@palantir.com>
2018-11-10 18:39:34 -0800

    Start new year chaos

---
+++ javascript/new-year-chaos.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumBribes function below.
+function minimumBribes(q) {
+
+
+}
+
+function main() {
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine(), 10);
+
+        const q = readLine().split(' ').map(qTemp => parseInt(qTemp, 10));
+
+        minimumBribes(q);
+    }
+}
