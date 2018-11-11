5078430bdfa86fdee933e9ccf992a66a05cdf739
Rootul Patel <rootulp@palantir.com>
2018-11-10 20:31:26 -0800

    Solve new year chaos (python)

--- python/new_year_chaos.py
+++ python/new_year_chaos.py
@@ -18,8 +18,18 @@ def invalid(finalLine):
 def didBribeMoreThanTwoPeople(person, index):
     return index + 2 < person - 1
 
-def bubbleSort(finalLine):
-    return 3
+def bubbleSort(line):
+    swaps = 0
+    numberOfPeople = len(line)
+
+    for person in range(numberOfPeople):
+        for i in range(0, numberOfPeople - person - 1):
+            if line[i] > line[i + 1]:
+                line[i], line[i + 1] = line[i + 1], line[i]
+                swaps += 1
+
+    return swaps
+
 
 if __name__ == '__main__':
     t = int(input())
