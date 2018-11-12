30a1f158c3e70b9b5f03376a6f2dd023e58b9ff8
Rootul Patel <rootulp@palantir.com>
2018-11-11 17:12:46 -0800

    Start bonetrousle

---
+++ python/bonetrousle.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the bonetrousle function below.
+#
+def bonetrousle(n, k, b):
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nkb = input().split()
+
+        n = int(nkb[0])
+
+        k = int(nkb[1])
+
+        b = int(nkb[2])
+
+        result = bonetrousle(n, k, b)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
