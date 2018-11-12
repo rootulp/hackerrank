7e0b0fc6888e2fc0c8a3631ba09ce3b7de0b27d0
Rootul Patel <rootulp@palantir.com>
2018-11-11 18:06:05 -0800

    Fix for python float precision

--- python/bonetrousle.py
+++ python/bonetrousle.py
@@ -10,6 +10,7 @@ def bonetrousle(n, k, b):
     if (minimumValue(k, b) <= n <= maximumValue(k, b)):
         return boxesToBuy(n, k, b)
     else:
+        # -1 indicates there is no possible solution
         return [-1]
 
 # The minimum number of sticks that may be purchased
@@ -22,19 +23,24 @@ def minimumValue(k, b):
 # Equivalant to: (k - b + 1) ... (k - 2) + (k -1) + k
 # See: https://en.wikipedia.org/wiki/Arithmetic_progression#Sum
 def maximumValue(k, b):
-    return b * ((k - b + 1) + k) / 2
+    # Test case 12 was failing because float percision in Python is inaccurate
+    # for large values. To alleviate, perform floor division (//) rather than
+    # standard division (/) because the latter returns a float.
+    return b * ((k - b + 1) + k) // 2
 
 # One possible solution of boxes that sum to n
 def boxesToBuy(n, k, b):
-    boxes = list(range(1, b + 1))
+    initialBoxes = list(range(1, b + 1))
     (quotient, remainder) = divmod((n - int(minimumValue(k, b))),  b)
-    boxes = list(map(lambda box: box + quotient, boxes))
+
+    # Add quotient to initialBoxes
+    boxes = list(map(lambda box: box + quotient, initialBoxes))
+
+    # Add 1 to the last r boxes (which effectively adds the remainder)
     for i in range(len(boxes) - 1, len(boxes) - remainder - 1, -1):
         boxes[i] += 1
 
     return boxes
-
-
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
