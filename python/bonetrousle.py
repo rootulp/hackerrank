d4f92bfc3925372f4a8bb67eb869b9a7162a482d
Rootul Patel <rootulp@palantir.com>
2018-11-11 17:18:24 -0800

    Skeleton bonetrousle impl

--- python/bonetrousle.py
+++ python/bonetrousle.py
@@ -3,13 +3,27 @@
 import os
 import sys
 
-#
-# Complete the bonetrousle function below.
-#
+# n: the integer number of sticks to buy
+# k: the integer number of box sizes the store carries
+# b: the integer number of boxes to buy
 def bonetrousle(n, k, b):
-    #
-    # Write your code here.
-    #
+    if (minimumValue(k, b) <= n <= maximumValue(k, b)):
+        return boxesToBuy(n, k, b)
+    else:
+        return -1
+
+# The minimum number of sticks that may be purchased
+def minimumValue(k, b):
+    return 0
+
+# The maximum number of sticks that may be purchased
+def maximumValue(k, b):
+    return 100
+
+# One possible solution of boxes that sum to n
+def boxesToBuy(n, k, b):
+    return [0]
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
