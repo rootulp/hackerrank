e05cc3631f7247f670c9f912a0f371aba357bdd5
Rootul Patel <rootulp@palantir.com>
2018-12-02 14:38:58 -0800

    Start count triplets

---
+++ python/count_triplets.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countTriplets function below.
+def countTriplets(arr, r):
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nr = input().rstrip().split()
+
+    n = int(nr[0])
+
+    r = int(nr[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    ans = countTriplets(arr, r)
+
+    fptr.write(str(ans) + '\n')
+
+    fptr.close()
