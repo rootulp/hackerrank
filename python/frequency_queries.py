5bdde1d451649ec84a62899b48c40c8e00b2e4aa
Rootul Patel <rootulp@palantir.com>
2018-12-01 20:50:09 -0800

    Extract class OccurencesAndFrequencies

--- python/frequency_queries.py
+++ python/frequency_queries.py
@@ -7,36 +7,51 @@ import re
 import sys
 from collections import Counter
 
-# Complete the freqQuery function below.
+class OccurencesAndFrequencies:
+  """A data structure that optimizes for keeping
+  track of the number of occurences of a given value
+  and the frequencies of occurences"""
+
+  def __init__(self):
+    # Dict of value to occurences of that value
+    self.occurences = Counter()
+    # Dict of occurences to the frequency of occurence
+    self.frequencies = Counter()
+
+  def add(self, value):
+    if (self.occurences[value] > 0):
+      self.frequencies[self.occurences[value]] -= 1
+
+    self.occurences[value] += 1
+    self.frequencies[self.occurences[value]] += 1
+
+  def remove(self, value):
+    if (self.occurences[value] > 0):
+      self.frequencies[self.occurences[value]] -= 1
+      self.occurences[value] -= 1
+      self.frequencies[self.occurences[value]] += 1
+
+  def containsFrequencyOf(self, freq):
+    return self.frequencies[freq] > 0
+
 def freqQuery(queries):
-  output = []
-  array = []
-  occurences = Counter()
-  frequencies = Counter()
+  oAndF = OccurencesAndFrequencies()
+  queryOutput = []
 
   for operation, value in queries:
     if (operation == 1):
-      frequencies[occurences[value]] -= 1
-      occurences[value] += 1
-      frequencies[occurences[value]] += 1
+      oAndF.add(value)
 
     elif (operation == 2):
-      if (occurences[value] > 0):
-        frequencies[occurences[value]] -= 1
-        occurences[value] -= 1
-        frequencies[occurences[value]] += 1
+      oAndF.remove(value)
 
     elif (operation == 3):
-      if frequencies[value] > 0:
-        output.append(1)
+      if oAndF.containsFrequencyOf(value):
+        queryOutput.append(1)
       else:
-        output.append(0)
+        queryOutput.append(0)
 
-    # print(occurences)
-    # print(frequencies)
-
-
-  return output
+  return queryOutput
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
