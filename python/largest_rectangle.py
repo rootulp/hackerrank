cc62c8a4d98ee9f9b86aa0721890c1856f88d73e
Rootul Patel <rootulp@palantir.com>
2018-12-02 17:27:45 -0800

    Solve largest rectangle

--- python/largest_rectangle.py
+++ python/largest_rectangle.py
@@ -6,8 +6,28 @@ import random
 import re
 import sys
 
-# Complete the largestRectangle function below.
-def largestRectangle(h):
+# Referenced https://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+def largestRectangle(heights):
+    stack = list()
+    index = 0
+    largest_rectangle = 0
+
+    while index < len(heights):
+        if (not stack) or (heights[stack[-1]] <= heights[index]):
+            stack.append(index)
+            index += 1
+        else:
+            top_of_stack = stack.pop()
+            area = (heights[top_of_stack] * ((index - stack[-1] - 1) if stack else index))
+            largest_rectangle = max(largest_rectangle, area)
+
+    while stack:
+        top_of_stack = stack.pop()
+        area = (heights[top_of_stack] * ((index - stack[-1] - 1) if stack else index))
+        largest_rectangle = max(largest_rectangle, area)
+
+    return largest_rectangle
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
