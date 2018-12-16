0cdbdee83df6e2bc80b301e5dabfba5a9c97d9d0
Rootul Patel <rootulp@palantir.com>
2018-12-15 17:31:29 -0800

    Rename store to heap

--- python/ctci_find_the_running_median.py
+++ python/ctci_find_the_running_median.py
@@ -11,38 +11,38 @@ class MinHeap:
     """A lightweight wrapper around Python's Min heap implementation."""
 
     def __init__(self):
-        self.store = []
+        self.heap = []
 
     def heap_push(self, value):
-        heappush(self.store, value)
+        heappush(self.heap, value)
 
     def heap_pop(self):
-        return heappop(self.store)
+        return heappop(self.heap)
 
     def peek(self):
-        return self.store[0]
+        return self.heap[0]
 
     def __len__(self):
-        return len(self.store)
+        return len(self.heap)
 
     def __str__(self):
-        return str(self.store)
+        return str(self.heap)
 
 class MaxHeap:
 
     def __init__(self):
-        self.store = []
+        self.heap = []
 
     def heap_push(self, value):
         """Add value to the heap while maintaining the max heap property."""
-        self.store.append(value)
-        value_idx = len(self.store) - 1
+        self.heap.append(value)
+        value_idx = len(self.heap) - 1
         self.sift_up(self.get_parent_idx(value_idx), value_idx)
 
     def heap_pop(self):
         """Pop the value off the top of the heap while maintaining the heap property."""
-        self.swap(0, len(self.store) - 1)
-        result = self.store.pop()
+        self.swap(0, len(self.heap) - 1)
+        result = self.heap.pop()
         self.sift_down(0)
         return result
 
@@ -51,7 +51,7 @@ class MaxHeap:
            If they are in the incorrect order, swap them and sift up with the new parent.
            If they are in the correct order then stop sifting up.
         """
-        if (self.store[child_idx] > self.store[parent_idx]):
+        if (self.heap[child_idx] > self.heap[parent_idx]):
             self.swap(child_idx, parent_idx)
             return self.sift_up(self.get_parent_idx(parent_idx), parent_idx)
 
@@ -65,7 +65,7 @@ class MaxHeap:
         if (larger_child_idx is None):
             return
 
-        if(self.store[idx] < self.store[larger_child_idx]):
+        if(self.heap[idx] < self.heap[larger_child_idx]):
             self.swap(idx, larger_child_idx)
             return self.sift_down(larger_child_idx)
 
@@ -79,8 +79,8 @@ class MaxHeap:
         elif(right_child_idx is None):
             return left_child_idx
         else:
-            left_child = self.store[left_child_idx]
-            right_child = self.store[right_child_idx]
+            left_child = self.heap[left_child_idx]
+            right_child = self.heap[right_child_idx]
             return left_child_idx if left_child > right_child else right_child_idx
 
 
@@ -89,7 +89,7 @@ class MaxHeap:
            Returns None if the value at idx does not have a left child.
         """
         left_child_idx = 2 * idx + 1
-        if(left_child_idx >= len(self.store)):
+        if(left_child_idx >= len(self.heap)):
             return None
         return left_child_idx
 
@@ -98,7 +98,7 @@ class MaxHeap:
            Returns None if the value at idx does not have a right child.
         """
         right_child_idx = 2 * idx + 2
-        if(right_child_idx >= len(self.store)):
+        if(right_child_idx >= len(self.heap)):
             return None
         return right_child_idx
 
@@ -108,19 +108,18 @@ class MaxHeap:
 
     def swap(self, idx_1, idx_2):
         """Swap the values at position idx_1 and idx_2."""
-        self.store[idx_1], self.store[idx_2] = self.store[idx_2], self.store[idx_1]
+        self.heap[idx_1], self.heap[idx_2] = self.heap[idx_2], self.heap[idx_1]
 
     def peek(self):
         """Returns the value at the top of the heap"""
-        return self.store[0]
+        return self.heap[0]
 
     def __len__(self):
         """Returns the current heap length."""
-        return len(self.store)
+        return len(self.heap)
 
     def __str__(self):
-        return str(self.store)
-
+        return str(self.heap)
 
 
 class RunningMedian:
@@ -130,16 +129,16 @@ class RunningMedian:
         self.max_heap = MaxHeap()
 
     def push(self, value):
-        if (len(self.min_heap) == 0 and len(self.max_heap) == 0):
+        # Add value to max_heap
+        self.min_heap.heap_push(value)
+        value = self.min_heap.heap_pop()
+        self.max_heap.heap_push(value)
+        value = self.max_heap.heap_pop()
+
+        if (len(self.min_heap) <= len(self.max_heap)):
             self.min_heap.heap_push(value)
         else:
-            median = self.get_median()
-            if (value <= median):
-                self.max_heap.heap_push(value)
-            elif (value > median):
-                self.min_heap.heap_push(value)
-
-        self.balance_heaps()
+            self.max_heap.heap_push(value)
 
         # print("Heaps look like: ")
         # print("MaxHeap: " + str(self.max_heap))
@@ -160,14 +159,14 @@ class RunningMedian:
 
     def get_median(self):
         """Returns the current median."""
-        if (len(self.min_heap) == len(self.max_heap)):
-            return (self.min_heap.peek() + self.max_heap.peek()) / 2.0
+        if (len(self.min_heap) == len(self.max_heap) == 0):
+            return 0
         elif (len(self.min_heap) > len(self.max_heap)):
             return self.min_heap.peek()
         elif (len(self.min_heap) < len(self.max_heap)):
             return self.max_heap.peek()
-        else:
-            return 0
+        elif (len(self.min_heap) == len(self.max_heap)):
+            return (self.min_heap.peek() + self.max_heap.peek()) / 2.0
 
 
 if __name__ == '__main__':
