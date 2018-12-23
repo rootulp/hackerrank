56bdf7b58f149f79c36d21912ba5f957192b8aad
Rootul Patel <rootulp@palantir.com>
2018-12-23 17:28:32 -0500

    Re-implement with merge sort

--- python/ctci-merge-sort.py
+++ python/ctci-merge-sort.py
@@ -6,27 +6,59 @@ import random
 import re
 import sys
 
+
+
 def countInversions(arr):
-    return bubbleSort(arr)
+    global COUNT_INVERSIONS
+    COUNT_INVERSIONS = 0
+    mergeSort(arr)
+    return COUNT_INVERSIONS
 
 
-# Sort array via bubble sort and return the number of swaps performed
-def bubbleSort(arr):
-    swaps = 0
+def mergeSort(arr):
+    if (len(arr) <= 1):
+        return arr
 
-    performedSwapInCurrentPass = True # Set to True initially to trigger first pass
-    while (performedSwapInCurrentPass == True):
-        performedSwapInCurrentPass = False
-        for index in range(len(arr) - 1):
-            if arr[index] > arr[index + 1]:
-                arr[index], arr[index + 1] = arr[index + 1], arr[index]
-                swaps += 1
-                performedSwapInCurrentPass = True
+    middle = len(arr) // 2
+    left = mergeSort(arr[:middle])
+    right = mergeSort(arr[middle:])
+    return merge(left, right)
 
-    return swaps
+
+def merge(arrLeft, arrRight):
+    mergedArray = []
+    leftIndex = 0
+    rightIndex = 0
+    global COUNT_INVERSIONS
+
+    while(leftIndex < len(arrLeft) and rightIndex < len(arrRight)):
+        if(arrLeft[leftIndex] <= arrRight[rightIndex]):
+            mergedArray.append(arrLeft[leftIndex])
+            leftIndex += 1
+        else:
+            mergedArray.append(arrRight[rightIndex])
+            rightIndex += 1
+            COUNT_INVERSIONS += len(arrLeft) - leftIndex
+
+    # If there are any remaining elements in arrLeft
+    # Append them to mergedArray
+    while(leftIndex < len(arrLeft)):
+        mergedArray.append(arrLeft[leftIndex])
+        leftIndex += 1
+        # COUNT_INVERSIONS += 1
+
+    # If there are any remaining elements in arrLeft
+    # Append them to mergedArray
+    while(rightIndex < len(arrRight)):
+        mergedArray.append(arrRight[rightIndex])
+        rightIndex += 1
+
+    # print("Merged array: {}".format(mergedArray))
+    return mergedArray
 
 
 if __name__ == '__main__':
+
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     num_test_cases = int(input())
     for _test_case in range(num_test_cases):
