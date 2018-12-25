1d192feb7fe91c43a4503b501176937121a84940
Rootul Patel <rootulp@palantir.com>
2018-12-25 11:59:01 -0500

    Solve lowest common ancestor

--- python/binary-search-tree-lowest-common-ancestor.py
+++ python/binary-search-tree-lowest-common-ancestor.py
@@ -47,18 +47,12 @@ class Node:
 '''
 
 def lca(root, v1, v2):
-    return root
+    if (v1 <= root.info <= v2 or v2 <= root.info <= v1):
+        return root
+    elif (root.info < v1 and root.info < v2):
+        return lca(root.right, v1, v2)
+    elif (root.info > v1 and root.info > v2):
+        return lca(root.left, v1, v2)
 
 
 tree = BinarySearchTree()
-t = int(input())
-
-arr = list(map(int, input().split()))
-
-for i in range(t):
-    tree.create(arr[i])
-
-v = list(map(int, input().split()))
-
-ans = lca(tree.root, v[0], v[1])
-print (ans.info)
