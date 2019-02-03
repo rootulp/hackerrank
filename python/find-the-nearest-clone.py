685706fa19d05411849f93e85140c7856db43c5e
Rootul Patel <rootulp@palantir.com>
2019-02-03 15:17:45 -0800

    Start find the nearest clone

---
+++ python/find-the-nearest-clone.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the findShortest function below.
+
+#
+# For the weighted graph, <name>:
+#
+# 1. The number of nodes is <name>_nodes.
+# 2. The number of edges is <name>_edges.
+# 3. An edge exists between <name>_from[i] to <name>_to[i].
+#
+#
+def findShortest(graph_nodes, graph_from, graph_to, ids, val):
+    # solve here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    graph_nodes, graph_edges = map(int, input().split())
+
+    graph_from = [0] * graph_edges
+    graph_to = [0] * graph_edges
+
+    for i in range(graph_edges):
+        graph_from[i], graph_to[i] = map(int, input().split())
+
+    ids = list(map(int, input().rstrip().split()))
+
+    val = int(input())
+
+    ans = findShortest(graph_nodes, graph_from, graph_to, ids, val)
+
+    fptr.write(str(ans) + '\n')
+
+    fptr.close()
