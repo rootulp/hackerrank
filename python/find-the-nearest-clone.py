4d6ddd1a4c695e14594ef38325f99ee6b0da2bb6
Rootul Patel <rootulp@palantir.com>
2019-02-03 15:22:49 -0800

    Improve input handling

--- python/find-the-nearest-clone.py
+++ python/find-the-nearest-clone.py
@@ -6,36 +6,28 @@ import random
 import re
 import sys
 
-# Complete the findShortest function below.
 
-#
-# For the weighted graph, <name>:
-#
-# 1. The number of nodes is <name>_nodes.
-# 2. The number of edges is <name>_edges.
-# 3. An edge exists between <name>_from[i] to <name>_to[i].
-#
-#
-def findShortest(graph_nodes, graph_from, graph_to, ids, val):
-    # solve here
+class Node:
+    pass
+
+class Graph:
+    pass
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    graph_nodes, graph_edges = map(int, input().split())
+    num_nodes, num_edges = map(int, input().split())
+    edges = []
 
-    graph_from = [0] * graph_edges
-    graph_to = [0] * graph_edges
+    for i in range(num_edges):
+        edge = list(map(int, input().split()))
+        edges.append(edge)
 
-    for i in range(graph_edges):
-        graph_from[i], graph_to[i] = map(int, input().split())
+    color_ids = list(map(int, input().rstrip().split()))
+    color_to_find = int(input())
 
-    ids = list(map(int, input().rstrip().split()))
+    graph = Graph(num_nodes, num_edges, edges, color_ids)
+    result = graph.smallest_path_length(color_to_find)
 
-    val = int(input())
-
-    ans = findShortest(graph_nodes, graph_from, graph_to, ids, val)
-
-    fptr.write(str(ans) + '\n')
-
+    fptr.write(str(result) + '\n')
     fptr.close()
