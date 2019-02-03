6aea02789018df5aff64aaf6084b19ad4d442c6b
Rootul Patel <rootulp@palantir.com>
2019-02-03 13:50:53 -0800

    Add helpers for graph initialization

--- python/breadth-first-search-shortest-reach.py
+++ python/breadth-first-search-shortest-reach.py
@@ -18,20 +18,29 @@ class Node:
         self.neighbors.append(neighbor_id)
 
     def __repr__(self):
-        return "Node ID: {}, Neighbors: {}".format(self.node_id, self.neighbors)
+        return "Node ID: {}, Neighbors: {}, Visited: {}".format(self.node_id, self.neighbors, self.visited)
 
 class Graph:
 
     def __init__(self, num_nodes, edges):
         self.num_nodes = num_nodes
+        self.edges = edges
+
         self.nodes = []
 
         for node_id in range(num_nodes):
-            self.nodes.append(Node(node_id + 1))
+            self.add_node(node_id)
 
-        for node_a, node_b in edges:
-            self.nodes[node_a - 1].add_neighbor(node_b)
-            self.nodes[node_b - 1].add_neighbor(node_a)
+        for edge in edges:
+            node_a, node_b = edge
+            self.add_edge(node_a - 1, node_b - 1)
+
+    def add_node(self, node_id):
+        self.nodes.append(Node(node_id))
+
+    def add_edge(self, node_a, node_b):
+        self.nodes[node_a].add_neighbor(node_b)
+        self.nodes[node_b].add_neighbor(node_a)
 
     def shortest_reach(self, starting_node):
         self.mark_all_nodes_unvisited()
@@ -42,9 +51,9 @@ class Graph:
         while nodes_to_visit:
             current_node, depth = nodes_to_visit.popleft()
             current_node.visited = True
-            shortest_reach_to_nodes[current_node.node_id - 1] = 6 * depth
+            shortest_reach_to_nodes[current_node.node_id] = 6 * depth
             for node_id in current_node.neighbors:
-                node = self.nodes[node_id - 1]
+                node = self.nodes[node_id]
                 if node.visited == False:
                     nodes_to_visit.append((node, depth + 1))
 
@@ -69,7 +78,6 @@ if __name__ == '__main__':
         starting_node = int(input())
 
         graph = Graph(num_nodes, edges)
-        # print(graph.nodes)
         shortest_reach = graph.shortest_reach(starting_node)
 
         fptr.write(' '.join(map(str, shortest_reach)))
