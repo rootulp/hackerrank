6451808c2dfb3d207bdd69c8aa138554f52cf5ba
Rootul Patel <rootulp@palantir.com>
2019-02-09 15:03:15 -0800

    Include dev comment on solution

--- python/common-child.py
+++ python/common-child.py
@@ -7,7 +7,8 @@ import re
 import sys
 
 # See https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
-def commonChild(s1, s2):
+# This solution creates the matrix described in "Traceback approach"
+def common_child(s1, s2):
     matrix = [[0 for i in range(len(s2) + 1)] for j in range(len(s1)+ 1)]
 
     for row_i in range(len(s1)):
@@ -19,13 +20,10 @@ def commonChild(s1, s2):
 
     return matrix[len(s1)][len(s2)]
 
-
-
-
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     s1 = input()
     s2 = input()
-    result = commonChild(s1, s2)
+    result = common_child(s1, s2)
     fptr.write(str(result) + '\n')
     fptr.close()
