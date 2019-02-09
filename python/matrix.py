68a4ad02f2d553e27a3fd43bf764574ff2bcb1f1
Rootul Patel <rootulp@palantir.com>
2019-02-09 15:10:40 -0800

    Improve input handling

--- python/matrix.py
+++ python/matrix.py
@@ -6,31 +6,22 @@ import random
 import re
 import sys
 
-# Complete the minTime function below.
-def minTime(roads, machines):
+def min_time(roads, machines):
+    pass
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    nk = input().split()
-
-    n = int(nk[0])
-
-    k = int(nk[1])
+    num_cities, num_machines = map(int, input().split())
 
     roads = []
-
-    for _ in range(n - 1):
+    for _ in range(num_cities - 1):
         roads.append(list(map(int, input().rstrip().split())))
 
-    machines = []
+    machines = [] # these are cities machines are in
+    for _ in range(num_machines):
+        machines.append(int(input()))
 
-    for _ in range(k):
-        machines_item = int(input())
-        machines.append(machines_item)
-
-    result = minTime(roads, machines)
+    result = min_time(roads, machines)
 
     fptr.write(str(result) + '\n')
-
     fptr.close()
