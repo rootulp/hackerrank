1d2feff498f4ffc3befd893623e266317c34e119
Rootul Patel <rootulp@palantir.com>
2019-02-09 15:35:42 -0800

    impl any_machines_connected

--- python/matrix.py
+++ python/matrix.py
@@ -8,13 +8,17 @@ import sys
 
 class Matrix:
 
-    def __init__(self, roads, machines):
+    def __init__(self, roads, num_cities, machines):
         self.roads = roads
+        self.num_cities = num_cities
         self.machines = machines
         self.time = 0
 
-    # return an integer representing the minimum time required to disrupt the connections among all machines.
     def min_time(self):
+        """
+        Return an integer representing the minimum time required to disrupt the
+        connections among all machines.
+        """
         while self.any_machines_connected():
             machine1, machine2 = self.get_connected_machines()
             roads = self.get_roads_connecting(machine1, machine2)
@@ -22,6 +26,18 @@ class Matrix:
             self.remove_road(minimal_road)
             self.add_time(minimal_road)
         return self.time
+
+
+    def any_machines_connected(self):
+        for machine in self.machines:
+            if self.is_connected_to_another_machine(machine):
+                return True
+        return False
+
+    def is_connected_to_another_machine(self, machine):
+        breadth_first_search
+
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -35,7 +51,7 @@ if __name__ == '__main__':
     for _ in range(num_machines):
         machines.append(int(input()))
 
-    matrix = Matrix(roads, machines)
+    matrix = Matrix(roads, num_cities, machines)
     result = matrix.min_time()
 
     fptr.write(str(result) + '\n')
