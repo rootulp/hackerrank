4d15693da157dc03337eae42bea51632f7e08809
Rootul Patel <rootulp@palantir.com>
2019-02-09 14:57:38 -0800

    Partially solve common child

--- python/common-child.py
+++ python/common-child.py
@@ -6,19 +6,23 @@ import random
 import re
 import sys
 
-# Complete the commonChild function below.
+# See https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
 def commonChild(s1, s2):
-    pass
+    if len(s1) == 0 or len(s2) == 0:
+        return 0
+    else:
+        if s1[-1] == s2[-1]:
+            return commonChild(s1[:-1], s2[:-1]) + 1
+        else:
+            temp1 = commonChild(s1[:-1], s2)
+            temp2 = commonChild(s1, s2[:-1])
+            return max(temp1, temp2)
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     s1 = input()
-
     s2 = input()
-
     result = commonChild(s1, s2)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
