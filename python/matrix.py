3d6abf328f9dfede4fd4b09bbc1b28eeadd90562
Rootul Patel <rootulp@palantir.com>
2019-02-09 15:21:13 -0800

    impl min_time

--- python/matrix.py
+++ python/matrix.py
@@ -11,11 +11,17 @@ class Matrix:
     def __init__(self, roads, machines):
         self.roads = roads
         self.machines = machines
+        self.time = 0
 
     # return an integer representing the minimum time required to disrupt the connections among all machines.
     def min_time(self):
-        pass
-
+        while self.any_machines_connected():
+            machine1, machine2 = self.get_connected_machines()
+            roads = self.get_roads_connecting(machine1, machine2)
+            minimal_road = self.get_minimal_road(roads)
+            self.remove_road(minimal_road)
+            self.add_time(minimal_road)
+        return self.time
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
