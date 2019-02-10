b49aab785153a7fd930b70b7042b1b8dad3fd56b
Rootul Patel <rootulp@palantir.com>
2019-02-09 19:09:42 -0800

    Solve Kruskals

--- python/kruskal-mst.py
+++ python/kruskal-mst.py
@@ -6,39 +6,61 @@ import random
 import re
 import sys
 
-#
-# Complete the 'kruskals' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts WEIGHTED_INTEGER_GRAPH g as parameter.
-#
+# From https://en.wikipedia.org/wiki/Kruskal%27s_algorithm#Pseudocode
+# KRUSKAL(G):
+# A = ∅
+# foreach v ∈ G.V:
+#    MAKE-SET(v)
+# foreach (u, v) in G.E ordered by weight(u, v), increasing:
+#    if FIND-SET(u) ≠ FIND-SET(v):
+#       A = A ∪ {(u, v)}
+#       UNION(u, v)
+# return A
 
-#
-# For the weighted graph, <name>:
-#
-# 1. The number of nodes is <name>_nodes.
-# 2. The number of edges is <name>_edges.
-# 3. An edge exists between <name>_from[i] and <name>_to[i]. The weight of the edge is <name>_weight[i].
-#
-#
+class Kruskal:
 
-def kruskals(g_nodes, g_from, g_to, g_weight):
-    # Write your code here
+    def __init__(self, num_nodes, edges):
+        self.num_nodes = num_nodes
+        self.edges = edges
+        self.sets = []
+        self.initialize_sets()
+
+    def initialize_sets(self):
+        for node in range(self.num_nodes):
+            self.sets.append(set([node + 1]))
+
+    def find_set(self, node):
+        for subset in self.sets:
+            if node in subset:
+                return subset
+
+    def union(self, set_a, set_b):
+        self.sets.remove(set_a)
+        self.sets.remove(set_b)
+        self.sets.append(set_a.union(set_b))
+
+    def weight_of_minimum_spanning_tree(self):
+        """
+        Returns an integer denoting the total weight of the Really Special SubTree.
+        """
+        total_weight = 0
+        edges_ascending = sorted(edges, key=lambda tup: tup[2])
+        for edge in edges_ascending:
+            node_a, node_b, weight = edge # unpack tuple
+            set_a = self.find_set(node_a)
+            set_b = self.find_set(node_b)
+            if set_a != set_b:
+                total_weight += weight
+                self.union(set_a, set_b)
+        return total_weight
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    num_nodes, num_edges = map(int, input().rstrip().split())
 
-    g_nodes, g_edges = map(int, input().rstrip().split())
+    edges = []
+    for i in range(num_edges):
+        edges.append(tuple(map(int, input().rstrip().split())))
 
-    g_from = [0] * g_edges
-    g_to = [0] * g_edges
-    g_weight = [0] * g_edges
-
-    for i in range(g_edges):
-        g_from[i], g_to[i], g_weight[i] = map(int, input().rstrip().split())
-
-    res = kruskals(g_nodes, g_from, g_to, g_weight)
-
-    # Write your code here.
-
-    fptr.close()
+    kruskal = Kruskal(num_nodes, edges)
+    print(str(kruskal.weight_of_minimum_spanning_tree()))
