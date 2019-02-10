ff07986f783731f541aab5d66d5cb1500310a1cf
Rootul Patel <rootulp@palantir.com>
2019-02-09 19:18:42 -0800

    Pseudocode for matrix

--- python/matrix.py
+++ python/matrix.py
@@ -6,53 +6,50 @@ import random
 import re
 import sys
 
+# Let's use an adapted form of Kruskal.
+# 1. Construct a set for each city.
+# 2. Iterate through edges in descending order of "cost to destroy".
+# 3. If the edge connects two machines, we must destroy it so add to total time.
+#    If the edge does not connect two machines, we do not need to destroy it.
+# 4. After iterating through all edges, return total time
+
+# From https://en.wikipedia.org/wiki/Kruskal%27s_algorithm#Pseudocode
+# KRUSKAL(G):
+# A = ∅
+# foreach v ∈ G.V:
+#    MAKE-SET(v)
+# foreach (u, v) in G.E ordered by weight(u, v), increasing:
+#    if FIND-SET(u) ≠ FIND-SET(v):
+#       A = A ∪ {(u, v)}
+#       UNION(u, v)
+# return A
+
 class Matrix:
 
     def __init__(self, roads, num_cities, machines):
         self.roads = roads
         self.num_cities = num_cities
         self.machines = machines
-        self.time = 0
 
     def min_time(self):
         """
         Return an integer representing the minimum time required to disrupt the
         connections among all machines.
         """
-        while self.any_machines_connected():
-            machine1, machine2 = self.get_connected_machines()
-            roads = self.get_roads_connecting(machine1, machine2)
-            minimal_road = self.get_minimal_road(roads)
-            self.remove_road(minimal_road)
-            self.add_time(minimal_road)
-        return self.time
-
-
-    def any_machines_connected(self):
-        for machine in self.machines:
-            if self.is_connected_to_another_machine(machine):
-                return True
-        return False
-
-    def is_connected_to_another_machine(self, machine):
-        breadth_first_search
+        pass
 
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
     num_cities, num_machines = map(int, input().split())
 
     roads = []
     for _ in range(num_cities - 1):
-        roads.append(list(map(int, input().rstrip().split())))
+        roads.append(tuple(map(int, input().rstrip().split())))
 
     machines = [] # these are cities machines are in
     for _ in range(num_machines):
         machines.append(int(input()))
 
     matrix = Matrix(roads, num_cities, machines)
-    result = matrix.min_time()
-
-    fptr.write(str(result) + '\n')
-    fptr.close()
+    print(matrix.min_time())
