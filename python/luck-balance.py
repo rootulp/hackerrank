b220aea07d233a608505ecd73f977a6920e867e0
Rootul Patel <rootulp@palantir.com>
2019-02-10 19:01:14 -0800

    Add dev comments and fix variable naming

--- python/luck-balance.py
+++ python/luck-balance.py
@@ -12,19 +12,25 @@ def max_luck_balance(contests, num_can_lose):
     after all the contests.
     """
     balance = 0
+
+    # We can lose all unimportant contests.
     unimportant_contests = [contest for contest in contests if contest[1] == 0]
     for contest_luck, _is_important in unimportant_contests:
         balance += contest_luck
 
+    # Sort the important contests in descending order of luck balance.
     important_contests = sorted([contest for contest in contests if contest[1] == 1], reverse=True)
 
-    contests_to_win = (important_contests)[:num_can_lose]
-    contests_to_lose = (important_contests)[num_can_lose:]
+    # We want to lose as many of the high balance contests as possible.
+    contests_to_lose = (important_contests)[:num_can_lose]
 
-    for contest_luck, _is_important in contests_to_win:
-        balance += contest_luck
+    # We must win the remaining contests.
+    contests_to_win = (important_contests)[num_can_lose:]
 
     for contest_luck, _is_important in contests_to_lose:
+        balance += contest_luck
+
+    for contest_luck, _is_important in contests_to_win:
         balance -= contest_luck
 
     return balance
