5b7c72f83e62839b7e7a200fe26f9f31941ab128
Rootul Patel <rootulp@palantir.com>
2019-02-10 18:13:40 -0800

    Rename variables for clarity

--- python/nested-list.py
+++ python/nested-list.py
@@ -1,13 +1,13 @@
 def get_students_with_grade(students, grade):
     return list(filter(lambda student: student[1] == grade , students))
 
+def get_second_lowest_grade(students):
+    unique_grades = set(map(get_grade, students))
+    ascending_grades = sorted(unique_grades)
+    return ascending_grades[1] # return second lowest grade
+
 def get_grade(student):
     return student[1]
-
-def get_second_lowest_grade(students):
-    grades = set(map(get_grade, students))
-    ascending_grades = sorted(grades)
-    return ascending_grades[1] # return second lowest grade
 
 if __name__ == '__main__':
     students = []
@@ -16,10 +16,8 @@ if __name__ == '__main__':
         score = float(input())
         students.append([name, score])
 
-    grade = get_second_lowest_grade(students)
-    # print("Grade {}".format(grade))
-    students_with_grade = get_students_with_grade(students, grade)
-    # print("Students with grade {}".format(students_with_grade))
+    second_lowest_grade = get_second_lowest_grade(students)
+    students_with_grade = get_students_with_grade(students, second_lowest_grade)
     sorted_students = sorted(students_with_grade)
-    for student in sorted_students:
-        print(student[0])
+    for name, _score in sorted_students:
+        print(name)
