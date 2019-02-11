316c86baa32351eb93869d63d63bf4eeafc41450
Rootul Patel <rootulp@palantir.com>
2019-02-10 20:12:02 -0800

    Refactor: extract methods for clarity

--- python/friend-circle-queries.py
+++ python/friend-circle-queries.py
@@ -8,12 +8,10 @@ import sys
 
 class FriendCircle:
 
-    POPULATION = 10 ** 9
-
     def __init__(self):
         self.circles = {}
         self.circle_sizes = {}
-        self.largest_friend_circle = 1
+        self.largest_circle_size = 1
 
     def befriend(self, friend_a, friend_b):
         self.maybe_init_circle(friend_a)
@@ -23,13 +21,14 @@ class FriendCircle:
         circle_b = self.find_circle(friend_b)
 
         if circle_a != circle_b:
-            self.circles[circle_b] = circle_a
-            new_circle_size = self.circle_sizes[circle_a] + self.circle_sizes[circle_b]
-            self.circle_sizes[circle_a] = new_circle_size
-            self.circle_sizes[circle_b] = new_circle_size
+            self.combine_circles(circle_a, circle_b)
 
-            if new_circle_size > self.largest_friend_circle:
-                self.largest_friend_circle = self.circle_sizes[circle_a]
+    def combine_circles(self, circle_a, circle_b):
+        self.circles[circle_b] = circle_a
+        new_circle_size = self.circle_sizes[circle_a] + self.circle_sizes[circle_b]
+        self.circle_sizes[circle_a] = new_circle_size
+        self.circle_sizes[circle_b] = new_circle_size
+        self.maybe_update_largest_circle_size(new_circle_size)
 
     def find_circle(self, person):
         if self.circles[person] == person:
@@ -43,6 +42,9 @@ class FriendCircle:
             self.circles[person] = person
             self.circle_sizes[person] = 1
 
+    def maybe_update_largest_circle_size(self, circle_size):
+        if circle_size > self.largest_circle_size:
+            self.largest_circle_size = circle_size
 
 if __name__ == '__main__':
     friend_circle = FriendCircle()
@@ -50,4 +52,4 @@ if __name__ == '__main__':
     for _ in range(num_queries):
         friend_a, friend_b = tuple(map(int, input().rstrip().split()))
         friend_circle.befriend(friend_a, friend_b)
-        print(friend_circle.largest_friend_circle)
+        print(friend_circle.largest_circle_size)
