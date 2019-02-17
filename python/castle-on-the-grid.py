328e08c5f465c81f70e8c8244c8f89a760bb6cca
Rootul Patel <rootulp@palantir.com>
2019-02-17 14:03:17 -0800

    Avoid timeout on test case 11 with minor optimization

--- python/castle-on-the-grid.py
+++ python/castle-on-the-grid.py
@@ -11,21 +11,24 @@ class CastleOnGrid:
         self.grid_size = grid_size
         self.start = start
         self.goal = goal
+        self.to_visit = deque()
+        self.visited = []
 
     def min_moves(self):
-        to_visit = deque()
-        visited = []
-
-        to_visit.append((self.start, 0))
-        visited.append(self.start)
+        self.to_visit.append((self.start, 0))
+        self.visited.append(self.start)
 
         current_coords = None
         while current_coords != self.goal:
-            current_coords, current_depth = to_visit.popleft()
+            current_coords, current_depth = self.to_visit.popleft()
             for move in self.possible_moves(current_coords):
-                if move not in visited:
-                    to_visit.append((move, current_depth + 1))
-                    visited.append(move)
+                # This is an optimization necessary to avoid timeouts on test
+                # case 11.
+                if move == self.goal:
+                    return current_depth + 1
+                else:
+                    self.to_visit.append((move, current_depth + 1))
+                    self.visited.append(move)
 
         return current_depth
 
@@ -34,24 +37,32 @@ class CastleOnGrid:
         for row_i in range(row + 1, self.grid_size):
             if self.grid[row_i][col] == self.CELL_BLOCKED_TOKEN:
                 break
+            elif (row_i, col) in self.visited:
+                continue
             else:
                 yield (row_i, col)
 
         for row_i in range(row - 1, -1, -1):
             if self.grid[row_i][col] == self.CELL_BLOCKED_TOKEN:
                 break
+            elif (row_i, col) in self.visited:
+                continue
             else:
                 yield (row_i, col)
 
         for col_i in range(col + 1, self.grid_size):
             if self.grid[row][col_i] == self.CELL_BLOCKED_TOKEN:
                 break
+            elif (row, col_i) in self.visited:
+                continue
             else:
                 yield (row, col_i)
 
         for col_i in range(col - 1, -1, -1):
             if self.grid[row][col_i] == self.CELL_BLOCKED_TOKEN:
                 break
+            elif (row, col_i) in self.visited:
+                continue
             else:
                 yield (row, col_i)
 
@@ -68,6 +79,4 @@ if __name__ == '__main__':
     goal = (coords[2], coords[3])
 
     castle_on_grid = CastleOnGrid(grid, grid_size, start, goal)
-    # print(castle_on_grid.possible_moves((1, 1)))
-    min_moves = castle_on_grid.min_moves()
-    print(min_moves)
+    print(castle_on_grid.min_moves())
