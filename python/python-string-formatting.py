0876ea5ad8e17cbc87f82c7c1565a59d2f679fbc
Rootul Patel <rootulp@palantir.com>
2019-02-16 18:59:55 -0800

    Solve string formatting

---
+++ python/python-string-formatting.py
@@ -0,0 +1,9 @@
+def print_formatted(number):
+    padding = len("{0:b}".format(number))
+    for num in range(1, number + 1):
+        print("{0:{width}d} {0:{width}o} {0:{width}x} {0:{width}b}".format(num, width=padding).upper())
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
