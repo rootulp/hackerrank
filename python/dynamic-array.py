788edfcddf50b46ee15be12e7d92595a249fdab0
Rootul Patel <rootulp@palantir.com>
2019-02-26 14:38:53 -0500

    Solve dynamic array

--- python/dynamic-array.py
+++ python/dynamic-array.py
@@ -6,27 +6,28 @@ import random
 import re
 import sys
 
-# Complete the dynamicArray function below.
-def dynamicArray(n, queries):
-    pass
+def dynamicArray(num_sequences, queries):
+    sequence_list = [[] for i in range(num_sequences)]
+    last_answer = 0
+
+    for query, x, y in queries:
+        sequence = get_sequence(num_sequences, x, last_answer, sequence_list)
+        if query == 1:
+            sequence.append(y)
+        elif query == 2:
+            index = y % len(sequence)
+            last_answer = sequence[index]
+            print(last_answer)
+
+def get_sequence(num_sequences, x, last_answer, sequence_list):
+    index = (x ^ last_answer) % num_sequences
+    return sequence_list[index]
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    nq = input().rstrip().split()
-
-    n = int(nq[0])
-
-    q = int(nq[1])
-
+    num_sequences, num_queries = tuple(map(int, input().rstrip().split()))
     queries = []
 
-    for _ in range(q):
+    for _ in range(num_queries):
         queries.append(list(map(int, input().rstrip().split())))
 
-    result = dynamicArray(n, queries)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
+    dynamicArray(num_sequences, queries)
