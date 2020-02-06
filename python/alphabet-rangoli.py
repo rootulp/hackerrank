f6ac5832340ab47e5dd663c9424f148ccab0353e
Rootul Patel <rootulp@palantir.com>
2020-02-06 14:17:29 -0500

    Solve rangoli

--- python/alphabet-rangoli.py
+++ python/alphabet-rangoli.py
@@ -1,6 +1,42 @@
+import string
+
+class Rangoli:
+
+    DASH = "-"
+    ALPHABET = string.ascii_lowercase
+    def __init__(self, size):
+        self.size = size
+        self.num_lines = self.size * 2 - 1
+        self.line_length = self.num_lines * 2 - 1
+        self.middle_line_num = (self.num_lines - 1) // 2
+
+    def __str__(self):
+        return "\n".join([self.line(line_number) for line_number in range(0, self.num_lines)])
+
+    def line(self, line_number):
+        return self.center_text(self.pattern(line_number))
+
+    def pattern(self, line_number):
+        num_lines_away_from_middle = abs(self.middle_line_num - line_number)
+        pattern = ""
+        for i in range(self.middle_line_num, num_lines_away_from_middle, -1):
+            pattern += self.ALPHABET[i] + self.DASH
+
+        pattern += self.ALPHABET[num_lines_away_from_middle]
+
+        for i in range(num_lines_away_from_middle + 1, self.middle_line_num + 1):
+            pattern += self.DASH + self.ALPHABET[i]
+
+        return pattern
+
+
+    def center_text(self, text):
+        return '{:{fill}^{line_length}}'.format(text, fill=self.DASH, line_length=self.line_length)
+
+
 def print_rangoli(size):
-    pass
-    # your code goes here
+    rangoli = Rangoli(size)
+    print(rangoli)
 
 if __name__ == '__main__':
     n = int(input())
