c458c3520f85f2e938aa3da3eef3be7bb2e1ffae
Rootul Patel <rootulp@palantir.com>
2020-04-01 12:46:03 -0400

    Solve two strings

---
+++ python/two-strings.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    if (len(set(s1).intersection(set(s2))) > 0):
+        return "YES"
+    else:
+        return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
