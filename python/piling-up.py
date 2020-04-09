1441654c46e08b7286999b6887e59c56fa238ff7
Rootul Patel <rootulp@palantir.com>
2020-04-09 13:54:52 -0400

    Remove returned pile b/c mutating directly

--- python/piling-up.py
+++ python/piling-up.py
@@ -4,7 +4,7 @@ from collections import deque
 def isVerticallyStackable(pile):
     vertical_stack = []
     while pile:
-        largest_cube, cube_sizes = remove_largest_cube_from_pile(pile)
+        largest_cube = remove_largest_cube_from_pile(pile)
         if vertical_stack == []:
             vertical_stack.append(largest_cube)
         else:
@@ -17,13 +17,11 @@ def isVerticallyStackable(pile):
 
 def remove_largest_cube_from_pile(cube_sizes):
     if(cube_sizes == []):
-        return (None, cube_sizes)
+        return None
     elif(cube_sizes[0] > cube_sizes[-1]):
-        largest_cube = cube_sizes.popleft()
-        return (largest_cube, cube_sizes)
+        return cube_sizes.popleft()
     else:
-        largest_cube = cube_sizes.pop()
-        return (largest_cube, cube_sizes)
+        return cube_sizes.pop()
 
 
 
