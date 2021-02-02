5bcfef7489c17883a695785cfae4230e4eec365b
Rootul Patel <rootulp@amazon.com>
2021-02-02 11:26:31 -0500

    Solve string intro

---
+++ java/java-strings-introduction/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+
+        System.out.println(A.length() + B.length());
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(capitalize(A) + " " + capitalize(B));
+    }
+
+    private static String capitalize(String word) {
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+
+}
