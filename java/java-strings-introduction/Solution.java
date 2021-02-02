c8e07fd8aa91a172a5a2218f80d4ae995ab1c228
Rootul Patel <rootulp@amazon.com>
2021-02-02 11:39:44 -0500

    Solve java substring

--- java/java-strings-introduction/Solution.java
+++ java/java-strings-introduction/Solution.java
@@ -4,9 +4,10 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String A = sc.next();
         String B = sc.next();
+        sc.close();
 
         System.out.println(A.length() + B.length());
         System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
---
+++ java/java-substring/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        in.close();
+
+        System.out.println(S.substring(start, end));
+    }
+}
